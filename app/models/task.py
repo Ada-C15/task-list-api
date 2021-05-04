5a60d705cc3ce7ef61a8bf79efc619b7b7db475d
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-04 16:36:00 -0700

    Task model update

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,7 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32))
+    description = db.Column(db.String(32))
+    completed_at = db.Column(db.DateTime)
+    # A task with a null value for completed_at has not been completed.
