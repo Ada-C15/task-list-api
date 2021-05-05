20269c892422d8c2a2048153a8bdacb72180ad83
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-04 20:31:13 -0700

    Adds put and delete methods to /tasks/<task_id>

--- app/routes.py
+++ app/routes.py
@@ -29,3 +29,38 @@ def handle_tasks():
         db.session.commit()
 
         return make_response(f"Task {new_task.title} successfully created", 201)
+
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+def handle_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        return {
+            "task": {
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+        }}
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+
+        db.session.commit()
+
+        return {
+            "task": {
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+        }}
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response(f"Task {task.title} successfully deleted")
