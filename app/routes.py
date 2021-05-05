44f1b7c188987361453ff89baff3dd13f3ed98f1
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-04 17:01:57 -0700

    create and register blueprint for tasks

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,7 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+from app.models.goal import Goal
+from flask import request, Blueprint, make_response, jsonify
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
