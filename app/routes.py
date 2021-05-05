a3cd102c64d7a16ade458b377f843e59c8409060
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-04 20:09:07 -0700

    Adds get method to get all tasks at /tasks

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,28 @@ from app import db
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["POST"])
+@tasks_bp.route("", methods=["POST", "GET"])
 def handle_tasks():
-    request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-    description=request_body["description"],
-    completed_at=request_body["completed_at"])
+    if request.method == "GET":
+        tasks = Task.query.all()
 
-    db.session.add(new_task)
-    db.session.commit()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+            })
+        return jsonify(tasks_response)
 
-    return make_response(f"Task {new_task.title} successfully created", 201)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task(title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"])
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response(f"Task {new_task.title} successfully created", 201)
