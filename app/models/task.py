51d2297256ea700a70bfeec37386b86fa92e6603
maite <arp.maite@gmail.com>
2021-05-04 23:57:36 -0700

    started work on endpoints for POST, GET, DELETE, and PUT

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,15 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True) # if null task hasnt been completed
+
+    def to_json(self):
+        return {
+            "task_id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at != None
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,59 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import jsonify
+from flask import request, make_response
 
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.route("", methods=["POST"])
+def handle_tasks():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"], 
+    description=request_body["description"], 
+    completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+    return make_response({"task":new_task.to_json()}, 201)
+
+
+@tasks_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_json())
+    return jsonify(tasks_response), 200 
+
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return {"details": "Task 1 \"Go on my daily walk 🏞\" successfully deleted"}
+    #something for a 404 here
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        return make_response({"task":task.to_json()}, 200)
+    # and here too
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        new_data = request.get_json()
+        task.title = new_data["title"]
+        task.description = new_data["description"]
+        task.completed_at = new_data["completed_at"]
+
+        db.session.commit()
+        return make_response({"task":task.to_json()}, 200)
+    #TODO: return 404 here
