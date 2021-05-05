0a5aa7eb4ac914f8f289e753fe7bb4f9a43955be
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-04 19:23:48 -0700

    Adds Task model and tasks blueprint for posting new task

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response, jsonify
+from app.models.task import Task
+from app import db
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def handle_tasks():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+    description=request_body["description"],
+    completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201)
