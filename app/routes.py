b7244896e7337d95442c4928231f32ae74865564
Whit Sundby <wksundby@gmail.com>
2021-05-05 19:23:43 -0700

    creates task_bp and registers blueprint

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import db
 from app.models.task import Task
 from flask import Blueprint, make_response, request, jsonify
 
-# tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 
 
