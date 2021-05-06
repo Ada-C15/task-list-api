53ed594d65f19738d1f00a7194c0b219c3dc17b8
alison <alimcginley@gmail.com>
2021-05-06 15:17:17 -0700

    adds asc desc functionality to GET

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import db
 from flask import request, Blueprint, make_response, jsonify
 from app.models.task import Task
+from sqlalchemy import desc, asc
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -8,28 +9,26 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 @tasks_bp.route("", methods = ["GET", "POST"])
 def handle_tasks():
     if request.method == "GET":
-        tasks = Task.query.all()
+        order = request.args.get("sort")
+        if order == "asc":
+            tasks_query = Task.query.order_by(Task.title).all()
+        else:
+            tasks_query = Task.query.order_by(Task.title.desc())
         tasks_response = []
-        for task in tasks:
-            if task.completed_at:
-                tasks_response.append({
+        for task in tasks_query:
+            tasks_response.append({
                 "id" : task.task_id,
                 "title" : task.title,
                 "description" : task.description,
-                "is_complete" : True
+                "is_complete" : bool(task.completed_at)
             })
-            else:
-                tasks_response.append({
-                "id" : task.task_id,
-                "title" : task.title,
-                "description" : task.description,
-                "is_complete" : False
-            })
-   
         return jsonify(tasks_response), 200
 
     elif request.method == "POST":
         request_body = request.get_json()
+        if "title" not in request_body.keys() or "description" not in request_body.keys() or "is_complete" not in request_body.keys():
+            return make_response({"details": "Invalid data"}, 400)
+        
         new_task = Task(
             title=request_body["title"],
             description = request_body["description"],
@@ -46,17 +45,12 @@ def handle_tasks():
 
             }}, 201
 
-@tasks_bp.route("/<task_id>", methods = ["GET", "PUT", "DELETE"])
+@tasks_bp.route("/<task_id>", methods = ["GET", "PUT", "DELETE", "PATCH"])
 def handle_task(task_id):
     task = Task.query.get(task_id)
     if task is None:
         return make_response("", 404)
     if request.method == "GET":
-        if task.completed_at:
-            task["is_complete"]: True
-        else:
-            task["is_complete"]: False
-        
         return {"task":{
             "id": task.task_id,
             "title": task.title,
@@ -64,3 +58,25 @@ def handle_task(task_id):
             "is_complete": bool(task.completed_at)
 
             }}
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+
+        db.session.commit()
+
+        return {"task":{
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": bool(task.completed_at)
+
+            }}
+    
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+
+        return {"details": f"Task {task_id} \"{task.title}\" successfully deleted"}
+    
