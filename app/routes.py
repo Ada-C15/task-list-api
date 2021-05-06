ef1dd60b715f8b71ff7eaf1ad91a5a143009b6df
Whit Sundby <wksundby@gmail.com>
2021-05-05 19:22:23 -0700

    adds Task model

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, make_response, request, jsonify
+
+# tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
 
