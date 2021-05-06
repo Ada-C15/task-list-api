9b061c14a3fea4e70e64b0c51d32b4c332b381b1
Leilani Allen <leilani.allen90@gmail.com>
2021-05-05 21:03:11 -0700

    Wave 1 passing

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,20 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+
+    # helper function to convert "complete" to true or false
+    def complete_helper(self):
+        if self.completed_at == None: # if empty return false, if not empty return true
+            complete = False
+        else:
+            complete = True
+        return complete
+    
+
+
+    
+    
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,112 @@
 from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import request
+from flask import request, Blueprint, make_response
+from flask import jsonify
 
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST", "GET"], strict_slashes=False)
+def tasks():
+    
+    if request.method == "GET":
+        task_title = request.args.get("title")
+        if task_title != None:
+            tasks = Task.query.filter_by(title=task_title)
+        else:
+            tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            complete=task.complete_helper()
+            tasks_response.append({
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": complete
+            })
+               
+        return jsonify(tasks_response)
+
+    
+    else:
+        request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+            return make_response(jsonify({"details": "Invalid data"}), 400)
+            
+        task = Task(title = request_body["title"],
+                            description = request_body["description"],
+                            completed_at = request_body["completed_at"])
+        
+
+        db.session.add(task)
+        db.session.commit()
+
+        complete=task.complete_helper()
+        
+        return make_response({
+                    "task": {
+                        "id": task.task_id,
+                        "title": task.title,
+                        "description": task.description,
+                        "is_complete": complete
+                }}), 201      
+
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+
+def handle_task(task_id):
+    
+
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        complete=task.complete_helper()
+        return ({
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": complete
+        }}), 200
+
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+
+        db.session.commit()
+
+        complete=task.complete_helper()
+        return ({
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": complete
+        }})
+
+    
+
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response({"details": (f"Task {task.task_id} \"Go on my daily walk 🏞\" successfully deleted")})
+    
+    
+    return {
+        "message": f"Task with id {task_task_id} was not found",
+        "success": False,
+    }, 404
+
+    
+    
+    
+    
+    
+    
