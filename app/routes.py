0b7d59ea07684f1fe47f058a931b0473b3d147dd
Whit Sundby <wksundby@gmail.com>
2021-05-05 19:42:41 -0700

    extends /tasks endpoint to accommodate GET method

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,45 @@ from flask import Blueprint, make_response, request, jsonify
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["POST"])
+@tasks_bp.route("", methods=["POST", "GET"])
 def handle_tasks():
+    if request.method == "POST":
+        request_body = request.get_json()
 
-    request_body = request.get_json()
+        new_task = Task(title=request_body['title'],
+                        description=request_body['description'],
+                        completed_at=request_body['completed_at'])
 
-    new_task = Task(title=request_body['title'],
-                    description=request_body['description'],
-                    completed_at=request_body['completed_at'])
+        db.session.add(new_task)
+        db.session.commit()
 
-    db.session.add(new_task)
-    db.session.commit()
-
-    response = {
-        "task": {
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": False
+        response = {
+            "task": {
+                "id": new_task.task_id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_complete": False
+            }
         }
-    }
 
-    return make_response(jsonify(response), 201)
+        return make_response(jsonify(response), 201)
+
+    elif request.method == "GET":
+
+        tasks = Task.query.all()
+        tasks_response = []
+
+        for task in tasks:
+            tasks_response.append({
+                "id": task.task_id,
+                "title": task.title,
+                "descroption":task.description,
+                "is_complete": is_task_complete(task)
+            })
+
+        return jsonify(tasks_response)
+
+def is_task_complete(task):
+    if not task.completed_at:
+        return False
+    return True
