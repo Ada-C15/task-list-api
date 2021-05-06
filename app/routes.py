1cc20c279e390409ad00de19e55255fa4ad79796
Sumitra <sumitrac2008@gmail.com>
2021-05-06 00:20:52 -0700

    Register Blueprint for goals

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,15 @@ from flask import request
 from flask import jsonify
 from flask import make_response
 from .models.task import Task
+from .models.goal import Goal
 from app import db 
+from datetime import datetime
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
+
+#wave_1
 @tasks_bp.route("", methods=["POST"])
 def create_tasks():
     request_body = request.get_json()
@@ -15,7 +20,7 @@ def create_tasks():
             "details": "Invalid data"
         }), 400
     else: 
-        task = Task(title=request_body["title"],
+        task = Task(title = request_body["title"],
             description = request_body["description"],
             completed_at = request_body["completed_at"])
 
@@ -34,7 +39,8 @@ def is_completed(request_body):
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
 
-    #sort titles by ace and desc order
+    #sort title by ace and desc order
+    #check wave_2 tests
     sort_by_title = request.args.get("sort")
     if sort_by_title == "asc":
         tasks = Task.query.order_by(Task.title.asc())
@@ -78,3 +84,20 @@ def handle_task(task_id):
         return make_response({
             "details": f"Task {task.task_id} \"{task.title}\" successfully deleted"
             })
+
+#wave_3 
+#creating custom endpoints with is_complete: True or False
+# @tasks_bp.route("/<task_id>/complete", methods=["PATCH"]) #update
+# def mark_complete(task_id):
+#     task = Task.query.get(task_id)
+
+#     if task.completed_at:
+
+
+
+#wave_4
+#use external Web API - slack 
+
+
+#wave_5
+#created second models for goals 
