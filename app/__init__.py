3b0126654aa846e13b2ee9ae1a910e9fe282b4d2
Ari <ari.go.org.7@gmail.com>
2021-05-05 18:56:49 -0700

    crated task model and added imports to init file and crated dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if test_config is None:
+    if test_config is None: #not test_config vs test_config=None
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
@@ -22,13 +22,21 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
+    ## this all grabs my models, where my routes are and my blueprints  to 
+    ## be able to run my app  #grabbing it all from everywhere to be able to run my app
+
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
-
+    from .routes import task_bp # ADDED THIS
+    from .routes import goal_bp  # ADDED THIS
+  
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
+    # Register Blueprints here   
+    app.register_blueprint(task_bp)  # ADDED THIS: need to create blue prints yet but they'll go here
+    app.register_blueprint(goal_bp)  # same as above
 
+    # IF ALL GOES WELL, HERE YOU APP IS READY TO 
     return app
--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,8 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # do i set it to autoincrement?
+    name = db.Column(db.String)
+    time = db.Column(db.DateTime)
+    date = db.Column(db.Date) # need to make sure it rounds to two decimals
+    created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
