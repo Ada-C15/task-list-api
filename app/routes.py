b189405bd145198f52b11b60f6b7007d89d71d20
brijhaywood <briyanahaywood@gmail.com>
2021-05-06 00:34:18 -0500

    Create a Valid task with null completed

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 def handle_tasks():
     if request.method == "POST":
         request_body = request.get_json()
-        new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=["completed_at"])
+        new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=request_body["completed_at"])
         db.session.add(new_task)
         db.session.commit()
 
