fc8b18e0e7c0699685a04a0962e4e9f6d4d925a3
Ruthie Newman <ruthienewman@protonmail.com>
2021-05-05 22:17:15 -0700

    Adds GET and POST endpoints for tasks.

--- app/__init__.py
+++ app/__init__.py
@@ -30,12 +30,12 @@ def create_app(test_config=None):
     # Register Blueprints here
 
     #task
-    from .routes import task_bp
-    app.register_blueprint(task_bp)
+    from .routes import tasks_bp
+    app.register_blueprint(tasks_bp)
 
     #goal
-    from .routes import goal_bp
-    app.register_blueprint(goal_bp)
+    from .routes import goals_bp
+    app.register_blueprint(goals_bp)
 
     from app.models.task import Task
     from app.models.goal import Goal
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,44 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
 
-task_bp = Blueprint("task", __name__, url_prefix="/task")
-
-goal_bp = Blueprint("goal", __name__, url_prefix="/goal")
-
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
 
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    if request.method == "GET":
+        tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "task_id" : task.task_id,
+                "description" : task.description,
+                "is_complete": 'false'
+            })
+        return jsonify(tasks_response)
+    
+    elif request.method == "POST":
+
+        request_body = request.get_json()
+        new_task = Task(
+            title = request_body["title"],
+            description = request_body["description"],
+            completed_at = request_body["completed_at"]
+        )
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response(f"Task {new_task.title} successfully created", 201)
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_task(task_id):
+    task = Task.query.get(task_id)
+
+    return {
+        "task_id" : task.task_id,
+        "description" : task.description,
+        "is_complete": 'false'
+    }
