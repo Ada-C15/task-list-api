84429aac3a53aab4434af177351460a566b374e0
Saejin Kwak Tanguay <saejinkwak@gmail.com>
2021-05-06 15:06:50 -0700

    Wave 1: completed set up of task model, added routes to get all tasks, post new task, get/update/delete task by id.

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,12 @@
-from flask import current_app
 from app import db
+from flask import current_app
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True) #autoincrement optional
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True) #nullable value
+
+#add jsonify class method
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,120 @@
-from flask import Blueprint
+
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
+import datetime
+
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["GET", "POST"], strict_slashes=False)
+
+def handle_task():
+
+    if request.method == "GET":
+
+        tasks = Task.query.all()
+        tasks_response = []
+
+        if tasks is None:
+            return make_response(f"{tasks_response}", 200)
+
+        else:
+            for task in tasks:
+                tasks_response.append({
+                    "id": task.task_id,
+                    "title": task.title,
+                    "description": task.description,
+                    "is_complete": bool(task.completed_at)
+                })
+
+            return jsonify(tasks_response)
+
+    elif request.method == "POST":
+
+        request_body = request.get_json()
+
+        if all(key in request_body for key in ("title", "description", "completed_at")):
+            new_task = Task(title=request_body["title"],
+                description=request_body["description"],
+                completed_at=request_body["completed_at"])
+
+            db.session.add(new_task)
+            db.session.commit()
+
+            tasks_response = {
+                    "id": new_task.task_id,
+                    "title": new_task.title,
+                    "description": new_task.description,
+                    "is_complete": bool(new_task.completed_at)
+                    }
+            dict_copy = tasks_response.copy()
+            response = {"task": dict_copy}
+
+
+            return jsonify(response), 201
+        
+        else:
+            response = {"details": "Invalid data"}
+            return jsonify(response), 400
+        
+
+    
+@task_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+
+def get_one_task(task_id):
+
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return jsonify(None), 404
+        
+    elif request.method == "GET":
+
+        tasks_response = {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": bool(task.completed_at)
+                }
+        dict_copy = tasks_response.copy()
+        response = {"task": dict_copy}
+
+        return jsonify(response)
+
+    elif request.method == "PUT":
+
+        form_data = request.get_json()
+
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at=form_data["completed_at"]
+
+        db.session.commit()
+
+        tasks_response = {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": bool(task.completed_at)
+                }
+
+        dict_copy = tasks_response.copy()
+        response = {"task": dict_copy}
+
+        return jsonify(response), 200
+    
+    elif request.method == "DELETE":
+
+        db.session.delete(task)
+        db.session.commit()
+
+        text = f"Task {task.task_id} \"{task.title}\" successfully deleted"
+        
+        response = {"details": text}
+
+        return jsonify(response), 200 
+
+
+
 
