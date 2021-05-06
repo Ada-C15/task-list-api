c74a808a676fa9f01fbf405e903e5fcf1b08ad50
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-05-06 11:21:30 -0700

    adds Get index for tasks and converts null to false

--- app/models/task.py
+++ app/models/task.py
@@ -3,15 +3,22 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title =  db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+
+    def convert_complete(self):
+        if self.completed_at == None:
+            complete = False
+        else:
+            complete = True
+        return complete
 
     def to_json(self):
         return {
             "task_id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "completed_at": self.completed_at
+            "completed_at": self.convert_complete()
         }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,39 @@
-from flask import Blueprint
+from flask import request, Blueprint, make_response
+from app import db
+from flask import jsonify
+from .models.task import Task
 
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+
+
+
+
+@task_bp.route("", methods=["POST"], strict_slashes=False)
+def add_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"],
+                    completed_at=request_body["completed_at"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({
+        
+        "task":{
+            "id": f"{new_task.task_id}",
+            "title": f"{new_task.title}",
+            "description": f"{new_task.description}",
+            "is_complete": f"{new_task.convert_complete()}"
+        }
+    }), 201
+
+@task_bp.route("", methods=["GET"], strict_slashes=False)
+def task_index():
+    tasks = Task.query.all()
+    task_response = []
+    for task in tasks:
+        task_response.append(task.to_json())
+    return jsonify(task_response), 200
+    
