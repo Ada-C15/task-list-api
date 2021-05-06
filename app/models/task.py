190b220a7245825068fc2628d1780854e359ab16
Raquel Mena <raquelmena03@gmail.com>
2021-05-06 15:07:03 -0500

    routes / task model updates

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,18 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+
+#write code to adjust completed at to true or false
+    def task_completed(self):
+        if self.completed_at:
+            return True
+        else:
+            return False
+
+    def return_task_json(self):
+        return  {"task": {
+        "id" : self.task_id,
+        "title" : self.title,
+        "description": self.description,
+        "is_complete":self.task_completed()
+        }}
--- app/routes.py
+++ app/routes.py
@@ -17,36 +17,44 @@ def create_task():
     db.session.add(task)
     db.session.commit()
 
-    return make_response({"task":{
-        "id": task.task_id,
-        "title": task.title,
-        "description": task.description,
-        "completed_at": task.completed_at
-    }})
+    return make_response(task.return_task_json(), 201)
 
+#get requests
+@tasks_bp.route("", methods=["GET"], strict_slashes=False)
+def get_tasks():
+    # if request.method == "GET":
+    #     id_query = request.args.get("id")
+    #     if id_query:
+    #         tasks = Task.query.filter_by(id=id_query)
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id" : task.task_id,
+            "title" : task.title,
+            "description": task.description,
+            "is_complete":task.task_completed()
+        })
+    return jsonify(tasks_response), 200
 
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+def get_single_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return make_response("", 404)
+    if request.method == "GET":
+        if task is None:
+            return make_response("none", 404)
+        return make_response(task.return_task_json())
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+        db.session.commit()
+        return make_response(task.return_task_json())
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response(f"details", 200)
 
-# #get requests
-# @tasks_bp.route("", methods=["GET"], strict_slashes=False)
-# def get_tasks():
-#     if request.method == "GET":
-#         id_query = request.args.get("id")
-#         if id_query:
-#             tasks = Task.query.filter_by(id=id_query)
-#     tasks_response = []
-#     for task in tasks:
-#         tasks_response.append({
-#             "id" : task.id,
-#             "title" : task.title,
-#             "description": task.description
-#             #"completed_at":task.completed_at
-#         })
-#     return jsonify(tasks_response), 200
-
-# @tasks_bp.route("/<task_id>", methods=["GET"])
-# def get_single_task(task_id):
-#     task = Task.query.get(task_id)
-#     if request.method == "GET":
-#         if task is None:
-#             return make_response("none", 404)
-#         return make_response(task)
