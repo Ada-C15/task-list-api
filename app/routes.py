7d080d3c90dee2e25c1b84e8aa0ab30415352e71
alison <alimcginley@gmail.com>
2021-05-06 10:37:14 -0700

    adds post functionality to tasks route

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods = ["GET"])
+@tasks_bp.route("", methods = ["GET", "POST"])
 def handle_tasks():
     if request.method == "GET":
         tasks = Task.query.all()
@@ -18,3 +18,15 @@ def handle_tasks():
                 "completed at" : task.completed_at
             })
         return jsonify(tasks_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task (
+            title = request_body["title"],
+            description = request_body["description"],
+            completed_at = request_body["completed at"]
+        )
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response(f"{new_task.name} has successfully been added to your task list", 200)
+
