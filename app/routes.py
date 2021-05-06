bfba6fb5bd52e5fe77662684ff02f5a09eb6f08f
Brittney Payne <ohoptional@gmail.com>
2021-05-06 15:54:01 -0700

    Added new functionality tocreate and put requests to update is_complete method

--- app/routes.py
+++ app/routes.py
@@ -38,8 +38,11 @@ def get_tasks():
             return make_response({"details": "Invalid data"}, 400)
         db.session.add(new_task)
         db.session.commit()
+        update_new_task = new_task.to_json()
+        update_new_task["is_complete"] = new_task.is_complete()
 
-        return make_response({"task": new_task.to_json()}, 201)
+
+        return make_response({"task": update_new_task}, 201)
 
 @task_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
 def get_single_task(task_id):
@@ -53,10 +56,13 @@ def get_single_task(task_id):
         request_body = request.get_json()
         task.title = request_body["title"]
         task.description = request_body["description"]
+        task.completed_at = request_body["completed_at"]
 
         db.session.commit()
+        updated_task = task.to_json()
+        updated_task["is_complete"] = task.is_complete()
 
-        return make_response({"task": task.to_json()}, 200)
+        return make_response({"task": updated_task}, 200)
     elif request.method == "DELETE":
         db.session.delete(task)
         db.session.commit()
@@ -79,3 +85,18 @@ def update_task_with_completion(task_id):
 
 
     return{"task": updated_task}
+
+
+@task_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def update_task_with_incomplete(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return make_response("", 404)
+    else: 
+        updated_task = task.to_json()
+        task.completed_at = None
+        db.session.commit()
+        updated_task["is_complete"] = task.is_complete()
+
+    return {"task": updated_task}
