7ac738a5cf047513b5997c6644072e22a935c175
Lacy Draper <ldrape1@gmail.com>
2021-05-06 12:08:39 -0700

    created routes for create task and get taskenpoints

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,22 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+
+
+    # helper function that gives is_complete a value to use in the is_complete and returns dict of task instances
+    def to_json(self):
+        if self.completed_at == None:
+            is_complete = False
+        else:
+            is_complete = True 
+            
+        return {
+        "id": self.task_id,
+        "title": self.title,
+        "description": self.description,
+        "is_complete": is_complete
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,53 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
+
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+#endpoint for new task entry post method 
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title = request_body["title"], description = request_body["description"],completed_at = request_body['completed_at'])
+      
+    db.session.add(new_task)
+    db.session.commit()
+        
+    return jsonify({
+  "task": {
+    "id": new_task.task_id,
+    "title": new_task.title,
+    "description": new_task.description,
+    "is_complete": False
+  }}), 201
+
+
+
+#endpoint for getting tasks
+@task_bp.route("", methods=["GET"])
+def tasks():
+    tasks = Task.query.all()
+    
+        
+    task_response = []
+    for task in tasks:
+        #using to_json helper function in tasks to retrun json format 
+        task_response.append(task.to_json()
+        )
+    #     {
+    #     "task_id": task.task_id,
+    #     "title": task.title,
+    #     "description": task.description,
+    #     "completed_at": task.completed_at
+    # }
+    return jsonify(task_response), 200
+
+# endpoint to get a cutomized response body by task_id
+@task_bp.route("/<task_id>", methods=["GET"])
+def task(task_id):
+    task = Task.query.get(task_id)
+    #task_dict = {'task': task}
+    # return task.to_json(), 200
+    return make_response({"task":task.to_json()}), 200
 
