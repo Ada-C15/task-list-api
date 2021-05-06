f78215b5960e2eef2478ee1a104c0c026f16f03f
Sahari <savilescante@gmail.com>
2021-05-06 17:46:06 -0500

    post1 test passed

--- app/models/task.py
+++ app/models/task.py
@@ -7,4 +7,11 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     title= db.Column(db.String)
     description= db.Column(db.String)
-    completed_at=db.Column(db.DateTime, nullable=True)
+    completed_at=db.Column(db.DateTime, nullable=True, default=None)
+
+#runs when there is a task completed
+    def is_complete(self):
+        if self.completed_at==None:
+            return False
+        else:
+            return True
--- app/routes.py
+++ app/routes.py
@@ -11,9 +11,19 @@ def handle_tasks():
     request_body = request.get_json()
     new_task = Task(title=request_body["title"],
                         description=request_body["description"],
-                        completed_at=null)
+                        completed_at=request_body["completed_at"])
 
-    db.session.add(new_task)
-    db.session.commit()
+    db.session.add(new_task) #adding data to db(a record/row) i.e git add
+    db.session.commit() # pushing it into the db i.e git commit + push
+    #we need to verify that the new data is in the db
+    get_task = Task.query.get(new_task.task_id) #brings data back
+    return  {
+        "task": {
+            "id": get_task.task_id,
+            "title": get_task.title,
+            "description":get_task.description,
+            "is_complete": get_task.is_complete()
+        }
+    }, 201
 
-    return  jsonify(new_task),201
+    
