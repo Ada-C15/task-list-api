ae62c5c5790f21372c8bcd58431a5dde23b6f74d
Ruthie Newman <ruthienewman@protonmail.com>
2021-05-05 19:21:19 -0700

    Adds task_bp and goal_bp Blueprints in routes.py and defines Task Model in task.py and defines Goal model in goal.py.

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,11 @@
 from flask import current_app
 from app import db
+from flask import Blueprint
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.String(50)) 
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 task_bp = Blueprint("task", __name__, url_prefix="/task")
 
-#goal_bp = Blueprint("goal", __name__, url_prefix="/goal")
+goal_bp = Blueprint("goal", __name__, url_prefix="/goal")
 
 
 
