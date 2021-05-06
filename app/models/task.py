6973a7c306228e43e0a2b2f75cd02340a1220fe8
Jittania <jittania@gmail.com>
2021-05-06 10:45:30 -0700

    completed basic setup and registered blueprint

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,8 @@ from app import db
 
 
 class Task(db.Model):
+    # can change task_id name, but all other columns must keep same names
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
-from flask import Blueprint
 
+from app import db
+from app.models.task import Task
+
+from flask import request, Blueprint, make_response
+
+# Blueprint instance - groups routes that start with /tasks.
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# START HERE============================
+# @blueprint_name.route("/endpoint/path/here", methods=["GET"])
+# def endpoint_name():
+#     my_beautiful_response_body = "Hello, World!"
+#     return my_beautiful_response_body
