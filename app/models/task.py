68a8e95d02181ef5a4ef682a4a1226d921dff3c2
Adelaide <adiainthesky@gmail.com>
2021-05-06 23:24:49 -0700

    refactor to move API response body into task constructor as method, and take 'is complete' out of task db and turn into method to be added into response body method

--- app/models/task.py
+++ app/models/task.py
@@ -7,7 +7,30 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
-    is_complete = False
+    # is_complete = False
 
-    if completed_at is True:
-        is_complete = True
+    # if completed_at is True:
+    #     is_complete = True
+
+    def completed(self):
+        is_complete = False
+        if self.completed_at is True:
+            is_complete = True
+        return is_complete    
+    
+    def api_response(self):
+        return (
+            {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": self.completed()
+                }
+            )
+            # {"task": {
+            #     "id": self.id,
+            #     "title": self.title,
+            #     "description": self.description,
+            #     "is_complete": self.completed()
+            #     }
+            # })            
