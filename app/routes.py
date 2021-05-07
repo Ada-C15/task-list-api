14226c6e773318c9b000f57400645346fd7dee10
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-05-06 20:50:10 -0700

    adds conditional to POST method, all tests in wave 1 passed

--- app/routes.py
+++ app/routes.py
@@ -9,9 +9,15 @@ task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 @task_bp.route("", methods=["POST"], strict_slashes=False)
 def add_task():
     request_body = request.get_json()
+    print(request_body)
+    if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+        return jsonify({
+        "details": "Invalid data"
+        }), 400
     new_task = Task(title=request_body["title"],
                     description=request_body["description"],
                     completed_at=request_body["completed_at"])
+
     db.session.add(new_task)
     db.session.commit()
 
