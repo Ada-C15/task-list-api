01624ada4b4021326fa58e038abe552a21c1d232
Abigail Charime <abigail.charime@gmail.com>
2021-05-07 10:02:39 -0700

    Created GET,POST,and PUT functionality to driver code/Wave 01

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,17 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_json(self):
+        # This method was created so that we do not have to write out the dictionary many times in the routes.py file. 
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            # Not sure if I should change this part to make if completed_at is null is_complete is False
+            "is_complete": False
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,76 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, request, make_response, jsonify
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("/<task_id>", methods=["GET","PUT", "DELETE"])
+def get_single_task(task_id):
+
+    task = Task.query.get(task_id)
+    # With the GET, POST and DELETE request if there is nothing we output this
+    if request == None or task == None:
+        return jsonify(None), 404
+    # This portion is the GET request for only one task
+    elif request.method == "GET":
+        return {"task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False}}, 200
+    elif request.method == "PUT":
+        # This portion is the PUT request for only one task
+        request_body = request.get_json()
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+        # Save action
+        db.session.commit()
+        return {"task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False}}, 200
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return {
+            "details": f"Task {task.task_id} \"{task.title}\" successfully deleted"
+            }, 200
+
+@tasks_bp.route("", methods=["GET"])
+def tasks_index():
+    # This portion is the GET request
+    tasks = Task.query.all()
+    if tasks == None:
+        return []
+    else:
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append(task.to_json())
+        return jsonify(tasks_response), 200
+
+
+@tasks_bp.route("", methods=["POST"])
+def tasks():
+    try:
+        # This portion is the POST request
+        request_body = request.get_json()
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at=request_body["completed_at"])
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response({
+            "task": {
+            "id": new_task.task_id,
+            "title": request_body["title"],
+            "description": request_body["description"],
+            "is_complete": False
+            }}, 201)
+    except KeyError:
+        return {
+            "details": "Invalid data"}, 400
+
 
