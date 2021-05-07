53a7c927c17e8ad064852dddf8d1f435c45a2fb9
marielaxcruz <marielacrtzcruz@gmail.com>
2021-05-07 12:01:34 -0700

    added a path route and asc and desc title order

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,30 @@ from datetime import datetime
 from flask import request, Blueprint, Response, jsonify, make_response
 from app import db
 from app.models.task import Task
+# wave #2 
+from sqlalchemy import asc, desc
+
+
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 @tasks_bp.route("", methods=["GET", "POST"])
 def all_tasks():
     if request.method == "GET":
-        tasks = Task.query.all()
+        sort = request.args.get("sort")
+        if sort == "asc": 
+            tasks = Task.query.order_by(asc("title"))
+        elif sort == "desc":
+            tasks = Task.query.order_by(desc("title"))     
+        else:
+            tasks = Task.query.all()
+        
         tasks_response =[]
         for task in tasks:
             tasks_response.append(task.to_json())
+        
         return jsonify(tasks_response)
+    
     elif request.method == "POST":
         request_body = request.get_json()
         # Create a Task: Invalid Task With Missing Data
@@ -55,3 +68,31 @@ def one_task_only(task_id):
         db.session.delete(task)
         db.session.commit()
         return make_response ({"details": f'Task {task.task_id} "{task.title}" successfully deleted'})
+
+
+
+
+#wave #3 
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def edit_one_task_complete(task_id):
+    task = Task.query.get(task_id)
+    if task == None:
+        return make_response("",404)
+        
+    task.completed_at = datetime.utcnow()
+    db.session.commit()
+
+    return make_response({"task" : task.to_json()}), 200
+
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def edit_one_task_incomplete(task_id):
+    task = Task.query.get(task_id)
+    
+    if task == None:
+        return make_response("",404)
+
+    task.completed_at = None
+    db.session.commit()
+    
+    return make_response({"task" : task.to_json()}), 200
