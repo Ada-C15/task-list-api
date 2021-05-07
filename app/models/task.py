710dca598787bf779dc403e6122cf165cb09ab04
SterlingSunshine <katrina.kimzey@gmail.com>
2021-05-07 10:40:05 -0700

    Wave 03 passed

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,10 @@
+# Task List API Project
+# Katrina Kimzey
+# Cohort 15 - Paper
+
 from flask import current_app
 from app import db
+from datetime import datetime
 
 
 class Task(db.Model):
@@ -7,12 +12,11 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
-    is_complete = db.Column(db.Boolean, default=False)
 
     def to_dict(self):
         return{
             "id" : self.task_id,
             "title" : self.title,
             "description" : self.description,
-            "is_complete" : self.is_complete
+            "is_complete" : bool(self.completed_at)
         }
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
+# Task List API Project
+# Katrina Kimzey
+# Cohort 15 - Paper
+
 from app import db
 from app.models.task import Task
 from flask import Blueprint, request, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import asc, desc
+from datetime import datetime
 
 tasks_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+# ============== All Tasks ========================
 
 @tasks_bp.route("", methods=["GET", "POST"])
 def handle_tasks():
@@ -43,6 +50,7 @@ def handle_tasks():
 
         return make_response({"task" : new_task.to_dict()}, 201)
 
+# ==================== Task by id ==============================
 
 @tasks_bp.route("/<active_id>", methods=["GET", "PUT", "DELETE"])
 def handle_task(active_id):
@@ -66,3 +74,23 @@ def handle_task(active_id):
         db.session.delete(task)
         db.session.commit()
         return jsonify({"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"})
+
+# ================= Task by id change completeness ======================
+
+@tasks_bp.route("/<active_id>/mark_complete", methods=["PATCH"])
+def update_to_complete(active_id):
+    task = Task.query.get_or_404(active_id)
+
+    task.completed_at = datetime.now()
+    db.session.commit()
+
+    return {"task" : task.to_dict()}
+
+@tasks_bp.route("/<active_id>/mark_incomplete", methods=["PATCH"])
+def update_to_incomplete(active_id):
+    task = Task.query.get_or_404(active_id)
+
+    task.completed_at = None
+    db.session.commit()
+
+    return {"task" : task.to_dict()}
