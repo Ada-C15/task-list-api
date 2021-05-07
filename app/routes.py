a840961b07fab2b824f29891497af5ff0ea85943
Hena <lee.henajane@gmail.com>
2021-05-06 22:50:30 -0500

    updates PUT endpoint method for Task model, creates Goal model, and implements CRUD operations for Goal model

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, request, jsonify
 from datetime import datetime
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
+# TASK CRUD OPERATIONS
 @tasks_bp.route("", methods = ["POST"], strict_slashes = False)
 def create_task():
     response_body = request.get_json()
-    
+
     if len(response_body) < 3:
         return jsonify({"details": f'Invalid data'}), 400
 
@@ -50,14 +53,14 @@ def view_task(task_id):
 @tasks_bp.route("/<task_id>", methods = ["PUT"], strict_slashes = False)
 def update_task(task_id):
     task = Task.query.get(task_id)
-    updated_data = request.get_json()
+    updated_task = request.get_json()
 
-    if not task or not updated_data:
+    if not task or not updated_task:
         return jsonify(None), 404
 
-    task.title = updated_data['title']
-    task.description = updated_data["description"]
-    task.completed_at = updated_data["completed_at"]
+    task.title = updated_task['title']
+    task.description = updated_task["description"]
+    task.completed_at = updated_task["completed_at"]
 
     db.session.commit()
     return jsonify({"task": task.task_to_json()})
@@ -90,3 +93,61 @@ def mark_task(task_id, completed):
         db.session.commit()
         return jsonify({"task": task.task_to_json()})
     return jsonify(None), 404
+
+# GOAL CRUD OPERATIONS
+@goals_bp.route("", methods = ["POST"], strict_slashes = False)
+def create_new_goal():
+    response_body = request.get_json()
+    if len(response_body) < 1:
+        return jsonify({"details": f'Invalid data'}), 400
+
+    new_goal = Goal(title = response_body["title"])
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return jsonify({"goal": new_goal.goal_to_json()}), 201
+
+@goals_bp.route("", methods = ["GET"], strict_slashes = False)
+def get_all_goals():
+    goals = Goal.query.all()
+    response_body = [] 
+    for goal in goals:
+        response_body.append(goal.goal_to_json())
+
+    return jsonify(response_body)
+
+@goals_bp.route("/<goal_id>", methods = ["GET"], strict_slashes = False)
+def view_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return jsonify(None), 404
+    return jsonify({"goal": goal.goal_to_json()})
+
+@goals_bp.route("/<goal_id>", methods = ["PUT"], strict_slashes = False)
+def update_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    updated_goal = request.get_json()
+    if not goal or not updated_goal:
+        return jsonify(None), 404
+
+    goal.title = updated_goal["title"]
+
+    db.session.commit()
+    return jsonify({"goal": goal.goal_to_json()})
+
+@goals_bp.route("/<goal_id>", methods = ["DELETE"], strict_slashes = False)
+def delete_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return jsonify(None), 404
+
+    db.session.delete(goal)
+    db.session.commit()
+    return jsonify({"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'})
+
+
+
+
