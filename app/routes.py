4674f456654becad0d45cefa3b90fc4a391a4399
terah <terahbruce@gmail.com>
2021-05-07 10:26:35 -0700

    Wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import db
 from .models.task import Task
 from flask import request, Blueprint, make_response, jsonify
+from sqlalchemy import desc, asc
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -17,23 +18,20 @@ def tasks():
             db.session.add(new_task)
             db.session.commit()
 
-            return jsonify({"task":{
-            "id":new_task.id,
-            "title":new_task.title,
-            "description":new_task.description ,
-            "is_complete":new_task.is_complete_func()
-            }}), 201
+            return jsonify({"task":new_task.to_dict()}), 201
 
     elif request.method == "GET":
-        tasks = Task.query.all()
+        sort_by = request.args.get("sort")
+        if sort_by == "asc": 
+            tasks = Task.query.order_by(Task.title.asc()).all()
+        elif sort_by == "desc":
+            tasks = Task.query.order_by(Task.title.desc()).all()
+        else: 
+            tasks = Task.query.all()
         tasks_response = []
         for task in tasks:
-            tasks_response.append({
-                "id": task.id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": task.is_complete_func()
-            })
+            tasks_response.append(task.to_dict())
+
         return jsonify(tasks_response), 200
 
 @task_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
@@ -44,12 +42,7 @@ def get_task(task_id):
         return make_response(), 404
     
     elif request.method == "GET":
-        return jsonify({"task":{
-                "id": task.id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": task.is_complete_func()
-            }}), 200
+        return jsonify({"task":task.to_dict()}), 200
 
     elif request.method == "DELETE":
         db.session.delete(task)
@@ -64,12 +57,7 @@ def get_task(task_id):
         task.completed_at = form_data["completed_at"]
         db.session.commit()
 
-        return jsonify({"task":{
-                "id": task.id,
-                "title": task.title,
-                "description": task.description,
-                "is_complete": task.is_complete_func()
-            }}), 200
+        return jsonify({"task":task.to_dict()}), 200
 
 
 
