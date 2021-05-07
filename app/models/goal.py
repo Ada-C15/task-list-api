a255f4b872bf3d03b2de590e346ec649afe9d721
Hannah Lumapas <lumapas.h@gmail.com>
2021-05-07 14:46:16 -0400

    sets up one-to-many relationship between the Parent model goal and Child model task

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,13 +1,21 @@
 from flask import current_app
 from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+# from .task import Task 
+# from sqlalchemy.ext.declarative import declarative_base
 
+# Base = declarative_base()
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    __tablename__ = "goal"
+    tasks = db.relationship("Task", backref="goals")
 
     def to_json(self): 
         return {
             "id": self.goal_id,
             "title": self.title
         }
+    
--- app/models/task.py
+++ app/models/task.py
@@ -1,13 +1,18 @@
 from flask import current_app
 from app import db
-
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from .goal import Goal 
 
 class Task(db.Model):
+    __tablename__ = "task"
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
-
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    # goals = db.relationship("Goal", back_populates="tasks")
+    
     def to_json(self): 
         return {
             "id": self.task_id,
