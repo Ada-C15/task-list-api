72e0554870782b239788ff94245bc58d55aa42b5
alison <alimcginley@gmail.com>
2021-05-07 17:32:08 -0700

    adds post functionality to goal

--- app/routes.py
+++ app/routes.py
@@ -95,12 +95,22 @@ def handle_goals():
     return jsonify(goals_response)
 
 @goals_bp.route("/<goal_id>", methods = ["GET"])
-def handle_goal(goal_id):
+def get_goal(goal_id):
     goal = Goal.query.get(goal_id)
     if goal is None:
         return make_response("", 404)
     else:
         return ({"goal" : goal.build_dict()}, 200)
+
+@goals_bp.route("", methods = ["POST"])
+def post_goal():
+    request_body = request.get_json()
+    new_goal = Goal(
+        title=request_body["title"])
+    db.session.add(new_goal)
+    db.session.commit()
+    return {"goal":new_goal.build_dict()}, 201
+
         
     
 
