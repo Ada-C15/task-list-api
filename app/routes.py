2463d06f545a81794e5c43fc78a3236e3df17671
Kristin <Kristineliseboyd13@gmail.com>
2021-05-07 18:45:22 -0700

    Passed 6 tests

--- app/routes.py
+++ app/routes.py
@@ -8,83 +8,65 @@ from flask import jsonify
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["GET"], strict_slashes=False)
-def get_task():
-    tasks = Task.query.all()
-    tasks_response = []
-    for task in tasks:
-        tasks_response.append({
-                "id": task.id,
+@tasks_bp.route("", methods=["POST", "GET"], strict_slashes=False)
+def handle_task():
+    if request.method == "GET":  
+        tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "id": task.task_id,
                 "title": task.title,
                 "description": task.description,
-                "completed_at": task.completed_at 
-            }, 200)
-    return jsonify(tasks_response)
+                "is_complete": False
+            })
+        return jsonify(tasks_response), 200
 
-@tasks_bp.route("", methods=["POST"], strict_slashes=False)
-def create_task():
-    request_body = request.get_json()
-    new_task = Task(title = request_body["title"], 
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task(title = request_body["title"], 
                     description = request_body["description"], 
                     completed_at = request_body["completed_at"])
-    db.session.add(new_task)
-    db.session.commit() 
+        db.session.add(new_task)
+        db.session.commit() 
 
-    return {
-        "task": new_task.to_json()
-    }, 201
-
-def is_int(value):
-    try: 
-        return int(value)
-    except ValueError:
-        return False
-
-@tasks_bp.route("/tasks/<task_id>", methods=["GET","PUT", "DELETE"], strict_slashes=False)
-def handle_task(task_id):
-    task = Task.query.get(task_id)
-    
-    if task is None:
-        return make_response("", 404)
-    
-    if not is_int(task_id):
         return {
-            "message": f"id {task_id} must be an integer", 
-            "success": False 
-        }, 400
+            "task":{
+                "id": new_task.task_id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_complete": False
+    }}, 201
 
+
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT"], strict_slashes=False)
+def task_by_id(task_id):
+    task = Task.query.get(task_id)
     if request.method == "GET":
         if task:
-            return task.to_json(), 200
+            return {
+                "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": False}
+        }, 200
 
-    elif request.method == "PUT":
-        if task: 
-            task_data = request.get_json()
-            task.title = task_data["title"]
-            task.description = task_data["description"]
-            db.session.commit()
-
-            return make_response(f"Task #{task_id} successfully updated", 200)
-
-    elif request.method == "DELETE":
+        else:
+            return (f"None", 404)
+    if request.method == "PUT":
         if task:
-            db.session.delete(task)
+            request_body = request.get_json()
+            task.title = request_body["title"]
+            task.description = request_body["description"]
+            task.completed_at = request_body["completed_at"]
             db.session.commit()
-            return make_response(f"Task #{task_id} successfully deleted", 200)
-
-    return make_response({
-            "message": f"Task with {task_id} was not found", 
-            "success": False 
-        }, 404)
-
-# @tasks_bp.route("/<task_id>", methods=["GET"], strict_slashes=False)
-# def single_task(task_id):
-#     task = Task.query.get(task_id)
-#     return make_response({
-#         "task": {
-#             "id": task.task_id,
-#             "title": task.title,
-#             "description": task.description,
-#             "completed_at": task.task_completed()}
-#         }, 200
-#     )
+            return { "task": {
+                        "id": task.task_id,
+                        "title": task.title,
+                        "description": task.description,
+                        "is_complete": False
+            }}, 200
+        else:
+            return (f"None", 404)
+    
