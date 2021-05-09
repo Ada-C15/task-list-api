4c0402648daf550686e7cb3b16148089e4572332
Manu Ponce <manuponce@Manus-MacBook-Pro.local>
2021-05-09 14:46:21 -0700

    updated app __init__, models, route, and tests

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,7 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)   # nullable allow an empty cell
+    __tablename__ = "tasks"    #specifically for flask migrate
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,113 @@
 from flask import Blueprint
+from app.models.task import Task
+from app import db
+from flask import request, Blueprint, make_response, Response, jsonify
 
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["GET", "POST"])
+def handle_tasks():  # NameError
+    if request.method == "GET":
+        tasks = Task.query.all()
+        tasks_response = []
+
+        for task in tasks:
+            if task.completed_at == None:
+                completed_at = False
+            else:
+                completed_at = True
+
+            tasks_response.append({
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": completed_at
+            })
+        return jsonify(tasks_response)
+
+    elif request.method == "POST":  # CRUD CREATE
+        # check for request body title and description, plus ensure both are strings
+        request_body = request.get_json()
+
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+            return {
+                "details": f"Invalid data"
+            }, 400
+
+        completed_at = request_body["completed_at"]
+
+        task = Task(
+            title=request_body["title"],
+            description=request_body["description"],
+            completed_at=request_body["completed_at"]
+        )
+
+        db.session.add(task)
+        db.session.commit()
+        
+        if task.completed_at == None:
+            completed_at = False
+        else:
+            completed_at = True
+        return {
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": completed_at
+            }
+        }, 201
+
+# getting 1 task
+@task_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+def handle_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task == None:
+        return make_response(f"Task {task_id} not found", 404)
+
+    if request.method == "GET":
+        if task.completed_at == None:
+            completed_at = False
+        else:
+            completed_at = True
+        return {
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": completed_at
+            }
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+
+        db.session.commit()
+
+        if task.completed_at == None:
+            completed_at = False
+        else:
+            completed_at = True
+        return {
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": completed_at
+            }
+        }
+
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        if task.completed_at == None:
+            completed_at = False
+        else:
+            completed_at = True
+        return {
+            "details": f"Task {task.task_id} \"{task.title}\" successfully deleted"
+        }
