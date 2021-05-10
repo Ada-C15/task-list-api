48a601a121eab32e9cdaf9a1efdc1cf77401fc69
Ruthie Newman <ruthienewman@protonmail.com>
2021-05-10 09:09:48 -0700

    Adds Goal model and instantiates a one to many relationship between Task and Goal instances.

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,6 +3,14 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(50))
-    #tasks = db.relationship('Goal', backref='person', lazy=True)
+    tasks = db.relationship('Task', backref='goal', lazy=True)
+
+    def create_goal_json(self):
+
+        return {
+                'id' : self.id,
+                'title' : self.title
+                #'tasks' : self.tasks
+        }
--- app/models/task.py
+++ app/models/task.py
@@ -4,8 +4,22 @@ from flask import Blueprint
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(50))
     description = db.Column(db.String(50)) 
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.id"), nullable=True)
     
+
+    def create_json(self):
+        if self.completed_at == None:
+            completed = False
+        else:
+            completed = True
+
+        return {
+                'id' : self.id,
+                'title' : self.title,
+                'description' : self.description,
+                'is_complete' : completed
+        }
