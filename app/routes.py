ab16820220167a4f2a9bb2a87c96ebd7ed2a74a4
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-10 16:33:45 -0700

    refactors GET & POST methods

--- app/routes.py
+++ app/routes.py
@@ -5,18 +5,40 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["POST"], strict_slashes=False)
-def tasks():
-    request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"]
-                    )
-    db.session.add(new_task)
-    db.session.commit()
+@tasks_bp.route("", methods=["GET","POST"], strict_slashes=False)
+def handle_tasks():
+    if request.method == "POST":
+        # try and except block for KeyError
+        try:
+            request_body = request.get_json()
 
-    return{
-        "success": True, 
-        "message": f"A new task : '{new_task.title}' has been successfully created"
-    }
+            new_task = Task(title=request_body["title"],
+                            description=request_body["description"],
+                            completed_at=request_body["completed_at"]
+                            )
 
+            db.session.add(new_task)
+            db.session.commit()
 
+            return {
+                "task": new_task.build_dict()
+            }, 201
+        
+        except KeyError:
+            return{"details": "Invalid data"}, 400
+
+    elif request.method == "GET":
+        task_title_from_url = request.args.get("title")
+        # search for task by title
+        if task_title_from_url:
+            tasks = Task.query.filter_by(title=task_title_from_url)
+        # all tasks
+        else:
+            tasks = Task.query.all()
+    
+            tasks_response = []
+
+            for task in tasks:
+                tasks_response.append(task.to_json())
+    
+            return jsonify(tasks_response), 200
