a298ba7defb671d7243e789d62332e36b05dc18b
SterlingSunshine <katrina.kimzey@gmail.com>
2021-05-09 23:24:02 -0700

    Later additions no longer break earlier tests

--- app/models/task.py
+++ app/models/task.py
@@ -12,13 +12,17 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
-    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True, default=None)
 
     def to_dict(self):
-        return{
+        ret_dict = {
             "id" : self.task_id,
             "title" : self.title,
             "description" : self.description,
             "is_complete" : bool(self.completed_at),
-            "goal_id" : self.goal_id
         }
+
+        if self.goal_id:
+            ret_dict["goal_id"] = self.goal_id
+
+        return ret_dict
