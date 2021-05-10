7d985975a9f3d8cd17dc768ef3e4b6b6a2a96d8f
Christian <Codedreamer20@gmail.com>
2021-05-10 12:07:09 -0700

    wave 1 succesfully completed

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,28 @@
 from app import db
-from app.models.task import Task
+from .models.task import Task
 from flask import Blueprint, request, make_response, jsonify
+
 
 task_bp = Blueprint("task", __name__, url_prefix='/tasks')
 
 @task_bp.route("", methods=["POST"])
 def post_task():
     request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"], 
+    if "title" in request_body and "description" in request_body and "completed_at" in request_body:
+        new_task = Task(title=request_body["title"],description=request_body["description"], 
                     completed_at=request_body["completed_at"])
-
+    else:   
+        return make_response({
+            "details": "Invalid data"
+        }), 400
+    
+    
     db.session.add(new_task)
     db.session.commit()
-    return jsonify({
-        "task":{
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": False
-        }      
-    }), 201
-    if "title" or "desription" or "completed_at" not in request_body:
-        return jsonify({
-            "details": "Invalid data"
-            }), 40
+    
+    
+    return jsonify({"task": new_task.to_json()}), 201
+    
 
 
 @task_bp.route("", methods=["GET"])
@@ -53,38 +51,29 @@ def handle_task(task_id):  # same name as parameter route
             "description": task.description,
             "is_complete": task.is_complete
         }      
-    }), 201
+    }), 200
     
-@task_bp.route("", methods=['DELETE', 'PUT'])
-def delete_put_tasks():
+@task_bp.route("/<task_id>", methods=['DELETE', 'PUT'])
+def delete_or_put_tasks(task_id):
     task = Task.query.get(task_id)
     if not task:
-        return 404
-    
-    if request.method == 'DELETE':
+        return "", 404
+    elif request.method == 'DELETE':
         db.session.delete(task)
         db.session.commit()
-        return ({
+        return jsonify({
             "details": f"Task {task_id} \"Go on my daily walk 🏞\" successfully deleted"
-        }, 200) 
-    
-
+            }), 200
+        
     elif request.method == 'PUT':
         request_body = request.get_json()
-        if "title" or "description" not in request_body:
-            return 404
-        if "title" in request_body:
-            task.title = request_body["title"]
-        if "description" in request_body:
-            task.description = request_body["description"]
+    
+        task.title = request_body["title"]
+    
+        task.description = request_body["description"]
         db.session.commit()
-        return jsonify({
-                "task": {
-                "id": 1,
-                "title": "Updated Task Title",
-                "description": "Updated Test Description",
-                "is_complete": false
-            }
-        }), 200
+        return jsonify({"task": task.to_json()}), 200
+        
+        
 
 
