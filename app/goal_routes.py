8030ed64ab757cbdfa9467519d3219a8c9d7913a
alison <alimcginley@gmail.com>
2021-05-11 11:18:43 -0700

    adds docstrings to goal_routes.py

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -12,23 +12,26 @@ load_dotenv()
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals") 
 
 @goals_bp.route("", methods = ["GET"])
-def handle_goals():
+def get_goals():
+    '''queries all goals from server'''
+
     goals_query = Goal.query.all()
-    goals_response = []
-    for goal in goals_query:
-        goals_response.append(goal.build_dict())
-    return jsonify(goals_response)
+
+    return jsonify([goal.build_dict() for goal in goals_query])
 
 @goals_bp.route("/<goal_id>", methods = ["GET"])
 def get_goal(goal_id):
-    goal = Goal.query.get(goal_id)
-    if goal is None:
-        return make_response("", 404)
-    else:
-        return ({"goal" : goal.build_dict()}, 200)
+    '''queries one goal from server using goal_id,
+    returns None, 404 using get_or_404 method if goal not found'''
+
+    goal = Goal.query.get_or_404(goal_id)
+
+    return ({"goal" : goal.build_dict()}, 200)
 
 @goals_bp.route("", methods = ["POST"])
 def post_goal():
+    '''posts one goal to database'''
+
     request_body = request.get_json()
     if "title" not in request_body.keys():
         return make_response({"details": "Invalid data"}, 400)
@@ -36,13 +39,14 @@ def post_goal():
         title=request_body["title"])
     db.session.add(new_goal)
     db.session.commit()
+
     return {"goal":new_goal.build_dict()}, 201
 
 @goals_bp.route("/<goal_id>", methods = ["PUT"])
 def update_goal(goal_id):
-    goal = Goal.query.get(goal_id)
-    if goal is None:
-        return make_response("", 404)
+    '''updates one goal in database'''
+
+    goal = Goal.query.get_or_404(goal_id)
     form_data = request.get_json()
     goal.title = form_data["title"]
     db.session.commit()
@@ -51,9 +55,9 @@ def update_goal(goal_id):
 
 @goals_bp.route("/<goal_id>", methods = ["DELETE"])
 def delete_goal(goal_id):
-    goal = Goal.query.get(goal_id)
-    if goal is None:
-        return make_response("", 404)
+    '''deletes one goal in database'''
+
+    goal = Goal.query.get_or_404(goal_id)
     db.session.delete(goal)
     db.session.commit()
 
@@ -61,23 +65,27 @@ def delete_goal(goal_id):
 
 @goals_bp.route("/<goal_id>/tasks", methods = ["POST"])
 def add_tasks_to_goals(goal_id):
-    goal = Goal.query.get(goal_id)
-    if goal:
-        request_body = request.get_json(goal)
-        for task_id in request_body["task_ids"]:
-            task = Task.query.get(task_id)
-            task.goal_id = goal_id
+    '''adds tasks to goals,
+    returns goal and associated task ids using list comprehension'''
 
-        db.session.commit()
+    goal = Goal.query.get_or_404(goal_id)
+    request_body = request.get_json(goal)
+    for task_id in request_body["task_ids"]:
+        task = Task.query.get(task_id)
+        task.goal_id = goal_id
 
-        return make_response(jsonify({"id": goal.goal_id, "task_ids": [task.task_id for task in goal.tasks]}))
+    db.session.commit()
+
+    return make_response(jsonify({"id": goal.goal_id, "task_ids": [task.task_id for task in goal.tasks]}))
 
 @goals_bp.route("/<goal_id>/tasks", methods = ["GET"])
 def get_tasks_for_goal(goal_id):
-    goal = Goal.query.get(goal_id)
-    if goal is None:
-        return make_response("", 404)
+    '''gets all tasks associated with one goal,
+    returns a goal and task dictionaries using list comprehension'''
+    
+    goal = Goal.query.get_or_404(goal_id)
     tasks = [task.build_dict() for task in goal.tasks]
     goal_dict = goal.build_dict()
     goal_dict["tasks"] = tasks
+
     return goal_dict
