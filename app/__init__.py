8efaf8bac8dce6d45a7ebe6bed1bdd53121511cf
Ari <ari.go.org.7@gmail.com>
2021-05-11 16:50:57 -0700

    created docstrings for model methods and created static method for task

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv # allows to bring .env variables
+import os # allows us to get the .env variables with method os.environ.get
 
 db = SQLAlchemy()
 migrate = Migrate()
-load_dotenv()
+load_dotenv()   # calls variables (that are text) from .ennv and converts
+                # them into real variables so git doesn't track the,
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if test_config is None: #not test_config vs test_config=None
+    if test_config is None: 
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
@@ -20,15 +21,13 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    ## this grabs my models, where my routes are and my blueprints  to 
-    ## be able to run my app  
-    ## grabbing it all from everywhere to be able to run my app
-
+    ## this grabs my models, where my routes are and my blueprints   
+    ## (grabbing it all from everywhere) to be able to run my app
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
-    from .routes import task_bp  # Importing routes from each model
-    from .routes import goal_bp  # same as above
+    from .routes import task_bp  # Importing all routes from each model
+    from .routes import goal_bp  # with the corresponding blue print
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/models/task.py
+++ app/models/task.py
@@ -3,10 +3,12 @@ from app import db
 from datetime import datetime   ## vs. with import datetime -  would be
                                 ## datetime.datetime in line 40
                                 ## like from random import randint 
+# if I want to do the from_jason method?, import this:
+# from flask import request # maybe I don't need it if I add arg to method
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  
-    title = db.Column(db.String) # name of task
+    title = db.Column(db.String) # name of task or title of task
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
     # adding one to many relationship  tasks to goal ==> dog to person 
@@ -14,57 +16,61 @@ class Task(db.Model):
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True) 
 
     def to_json_response(self):
+        '''
+        Converts a Task instance into JSON
+        Output: Returns a Python dictionary in the shape of JSON response 
+        that the API returns in the route that is called (GET).
+        '''
         return {"task": 
                         {"id": self.id,
                         "title": self.title,
                         "description": self.description,
                         "is_complete": bool(self.completed_at)}
-                } 
+                }
+                
     def task_to_json_response(self):
+        '''
+        Converts a Task's instance into JSON response
+        Output: Returns a Python dictionary in the shape JSON response
+        for only one task.
+        '''
         return {"id": self.id,
                 "title": self.title,
                 "description": self.description,
                 "is_complete": bool(self.completed_at)}
 
     def task_to_json_response_w_goal(self):
+        '''
+        Converts a Task's instance columns (atributes?) into JSON response including
+        the foreign key goal id.
+        Output: Returns a Python dictionary in the shape JSON response
+        for a task that is part of a goal.
+        '''
         return {"id": self.id,
                 "goal_id": self.goal_id,
                 "title": self.title,
                 "description": self.description,
                 "is_complete": bool(self.completed_at)}
 
-# A task's is_complete is true when there is a datetime for 
-# the task's completed_at value. A task's is_complete is 
-# false when there is a null/None value for the tasks's 
-# completed_at value.
-    def set_completion(self):   # if method is called, it changes completed_at 
-                                # with currentdate/time
+    def set_completion(self):   
+        '''
+        Updates the attribute completed_at of the instance of a Task
+        to the current date/time.
+        '''
         complete_time = (datetime.now()).strftime("%c")
         self.completed_at = complete_time 
 
+# TO DO WITH 
+# Create a class method in Task named from_json()
+# Converts JSON into a new instance of Task
+# Takes in a dictionary in the shape of the JSON our API receives in the create and update routes
+# Returns an instance of Task
 
-
-# references
-# 
-# https://github.com/SlackAPI/python-slack-sdk
-# https://api.slack.com/methods/chat.postMessage/code
-# https://pypi.org/project/requests/
-
-# import datetime
-# now = datetime.datetime.now()
-# print(now.strftime("%A"), now) 
-# ==> Friday 2021-05-07 16:26:45.456577
-
-# x = datetime.datetime.now()
-# print(x.strftime("%c")) ==> Fri May 07 09:29:22 2021 
-
-# print(x)
-# print(x.strftime("%A"))
-# https://www.w3schools.com/python/trypython.asp?filename=demo_datetime2
-# %a 	Weekday, short version 	Wed 	
-# %A 	Weekday, full version 	Wednesday
-# %c 	Local version of date and time 	Mon Dec 31 17:41:00 2018
-
-# https://stackoverflow.com/questions/15707532/import-datetime-v-s-from-datetime-import-datetime
+    @staticmethod
+    def from_json_to_task(request_body):
+        new_task = Task(title=request_body["title"],
+                description=request_body["description"],
+                completed_at = request_body["completed_at"])
+        return new_task
 
 
