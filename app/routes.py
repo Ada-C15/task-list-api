e29ad69c46e3619d4d386bcb6790d4f335ec6636
mairandomness <mairakodama@gmail.com>
2021-05-11 11:15:51 -0700

    refactor using get_or_404() method

--- app/routes.py
+++ app/routes.py
@@ -78,9 +78,7 @@ def invalid_post_request_body(request_body):
         return True
     if request_body["completed_at"] is not None:
         try:
-            # flask puts the datetime in rfc 1123 format and I could not for the
-            # life of me to find a way to confirm that the string is a valid datetime
-            # that wasnt kinda bad D:
+            # flask puts the datetime in rfc 1123 format and I had to use parser.parse
             dateutil.parser.parse(request_body["completed_at"])
         except dateutil.parser.ParserError:
             return True
@@ -94,11 +92,8 @@ def get_task_by_id(task_id):
     when the task is found
     Returns a 404 with no response body in case the task is not found
     """
-    task = Task.query.get(task_id)
-    if task:
-        return jsonify(task=task.as_dict())
-
-    return make_response(jsonify(None), 404)
+    task = Task.query.get_or_404(task_id)
+    return jsonify(task=task.as_dict())
 
 
 @tasks_bp.route("/<int:task_id>", methods=["DELETE"])
@@ -108,12 +103,11 @@ def delete_task(task_id):
     message
     Returns a 404 with no response body in case the task is not found
     """
-    task = Task.query.get(task_id)
-    if task:
-        db.session.delete(task)
-        db.session.commit()
-        return make_response(jsonify(details=f"Task {task.task_id} \"{task.title}\" successfully deleted"), 200)
-    return make_response(jsonify(None), 404)
+    task = Task.query.get_or_404(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+    return make_response(jsonify(details=f"Task {task.task_id} \"{task.title}\" successfully deleted"), 200)
 
 
 @tasks_bp.route("/<int:task_id>", methods=["PUT"])
@@ -126,19 +120,18 @@ def update_task(task_id):
 
     Returns a 404 with no response body in case the task is not found
     """
-    task = Task.query.get(task_id)
-    if task:
-        request_body = request.get_json()
+    task = Task.query.get_or_404(task_id)
 
-        if invalid_post_request_body(request_body):
-            return make_response({"details": "Invalid data"}, 400)
+    request_body = request.get_json()
 
-        task.title = request_body["title"]
-        task.description = request_body["description"]
-        task.completed_at = request_body["completed_at"]
-        db.session.commit()
-        return make_response(jsonify(task=task.as_dict()), 200)
-    return make_response(jsonify(None), 404)
+    if invalid_post_request_body(request_body):
+        return make_response({"details": "Invalid data"}, 400)
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body["completed_at"]
+    db.session.commit()
+    return make_response(jsonify(task=task.as_dict()), 200)
 
 
 @tasks_bp.route("/<int:task_id>/mark_complete", methods=["PATCH"])
@@ -148,15 +141,13 @@ def mark_task_complete(task_id):
     with the updated task and calls the send_task_notification funtion
     Returns a 404 with no response body in case the task is not found
     """
-    task = Task.query.get(task_id)
-    if task:
-        task.completed_at = datetime.now(timezone.utc)
-        db.session.commit()
+    task = Task.query.get_or_404(task_id)
 
-        send_slack_task_notification(task)
+    task.completed_at = datetime.now(timezone.utc)
+    db.session.commit()
 
-        return make_response(jsonify(task=task.as_dict()), 200)
-    return make_response(jsonify(None), 404)
+    send_slack_task_notification(task)
+    return make_response(jsonify(task=task.as_dict()), 200)
 
 
 def send_slack_task_notification(task):
@@ -185,12 +176,11 @@ def mark_task_incomplete(task_id):
     with the updated task
     Returns a 404 with no response body in case the task is not found
     """
-    task = Task.query.get(task_id)
-    if task:
-        task.completed_at = None
-        db.session.commit()
-        return make_response(jsonify(task=task.as_dict()), 200)
-    return make_response(jsonify(None), 404)
+    task = Task.query.get_or_404(task_id)
+
+    task.completed_at = None
+    db.session.commit()
+    return make_response(jsonify(task=task.as_dict()), 200)
 
 
 @goals_bp.route("", methods=["GET"])
@@ -244,11 +234,9 @@ def get_goal_by_id(goal_id):
     when the goal is found
     Returns a 404 with no response body in case the goal is not found
     """
-    goal = Goal.query.get(goal_id)
-    if goal:
-        return jsonify(goal=goal.as_dict())
+    goal = Goal.query.get_or_404(goal_id)
 
-    return make_response(jsonify(None), 404)
+    return jsonify(goal=goal.as_dict())
 
 
 @goals_bp.route("/<int:goal_id>", methods=["DELETE"])
@@ -258,12 +246,11 @@ def delete_goal(goal_id):
     message
     Returns a 404 with no response body in case the goal is not found
     """
-    goal = Goal.query.get(goal_id)
-    if goal:
-        db.session.delete(goal)
-        db.session.commit()
-        return make_response(jsonify(details=f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"), 200)
-    return make_response(jsonify(None), 404)
+    goal = Goal.query.get_or_404(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+    return make_response(jsonify(details=f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"), 200)
 
 
 @goals_bp.route("/<int:goal_id>", methods=["PUT"])
@@ -276,17 +263,16 @@ def update_goal(goal_id):
 
     Returns a 404 with no response body in case the goal is not found
     """
-    goal = Goal.query.get(goal_id)
-    if goal:
-        request_body = request.get_json()
+    goal = Goal.query.get_or_404(goal_id)
 
-        if "title" not in request_body:
-            return make_response({"details": "Invalid data"}, 400)
+    request_body = request.get_json()
 
-        goal.title = request_body["title"]
-        db.session.commit()
-        return make_response(jsonify(goal=goal.as_dict()), 200)
-    return make_response(jsonify(None), 404)
+    if "title" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+
+    goal.title = request_body["title"]
+    db.session.commit()
+    return make_response(jsonify(goal=goal.as_dict()), 200)
 
 
 @goals_bp.route("<int:goal_id>/tasks", methods=["POST"])
@@ -298,21 +284,20 @@ def add_tasks_to_goal(goal_id):
     If any of the expected field is missing in the post request body, it returns
     a 400 response indicating invalid data
     """
-    goal = Goal.query.get(goal_id)
-    if goal:
-        request_body = request.get_json()
+    goal = Goal.query.get_or_404(goal_id)
 
-        if "task_ids" not in request_body:
-            return make_response({"details": "Invalid data"}, 400)
+    request_body = request.get_json()
 
-        for task_id in request_body["task_ids"]:
-            task = Task.query.get(task_id)
-            task.goal_id = goal_id
+    if "task_ids" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
 
-        db.session.commit()
+    for task_id in request_body["task_ids"]:
+        task = Task.query.get(task_id)
+        task.goal_id = goal_id
 
-        return make_response(jsonify(task_ids=request_body["task_ids"], id=goal_id), 200)
-    return make_response(jsonify(None), 404)
+    db.session.commit()
+
+    return make_response(jsonify(task_ids=request_body["task_ids"], id=goal_id), 200)
 
 
 @goals_bp.route("<int:goal_id>/tasks", methods=["GET"])
@@ -322,10 +307,9 @@ def get_tasks_in_goal(goal_id):
 
     If the goal is not found returns a 404 response
     """
-    goal = Goal.query.get(goal_id)
-    if goal:
-        goal_dict = goal.as_dict()
-        goal_dict["tasks"] = [task.as_dict() for task in goal.tasks]
+    goal = Goal.query.get_or_404(goal_id)
 
-        return make_response(jsonify(goal_dict), 200)
-    return make_response(jsonify(None), 404)
+    goal_dict = goal.as_dict()
+    goal_dict["tasks"] = [task.as_dict() for task in goal.tasks]
+
+    return make_response(jsonify(goal_dict), 200)
