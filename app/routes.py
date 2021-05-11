81acb54b4b097fc735e9ecc507c3f7ac55023e2e
lisa1501 <halisasaipulla@gmail.com>
2021-05-11 11:50:29 -0700

    Re-organize Routes

--- app/routes.py
+++ app/routes.py
@@ -5,51 +5,44 @@ from flask import request, Blueprint, make_response, jsonify
 from datetime import datetime
 import os, requests
 
-
-
 tasks_bp = Blueprint("tasks",__name__,url_prefix="/tasks")
 goals_bp = Blueprint("goals",__name__,url_prefix="/goals")
 
-
-@tasks_bp.route("", methods=["POST"])
+#wave1
+@tasks_bp.route("", methods=["POST","GET"])
 def handle_tasks():
+    if request.method == "POST":
+        request_body = request.get_json()
+        
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body: 
+            return ({
+                "details":"Invalid data"
+            },400)
+        else:
+            task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at =request_body["completed_at"])
+            db.session.add(task)
+            db.session.commit()
 
-    request_body = request.get_json()
-    
-    if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body: 
-        return ({
-            "details":"Invalid data"
-        },400)
+            return make_response({"task":task.to_json()},201)
     else:
-        task = Task(title=request_body["title"],
-                    description=request_body["description"],
-                    completed_at =request_body["completed_at"])
-
-        db.session.add(task)
-        db.session.commit()
-
-        return make_response({"task":task.to_json()},201)
-
+        title_sort_query = request.args.get("sort")
     
-@tasks_bp.route("", methods=["GET"])
-def get_all_task():
+        if title_sort_query == "desc":
+            tasks = Task.query.order_by(Task.title.desc())
+        elif title_sort_query == "asc":
+            tasks = Task.query.order_by(Task.title.asc())
+        else:
+            tasks = Task.query.all()
 
-    title_sort_query = request.args.get("sort")
-    
-    if title_sort_query == "desc":
-        tasks = Task.query.order_by(Task.title.desc())
-    elif title_sort_query == "asc":
-        tasks = Task.query.order_by(Task.title.asc())
-    else:
-        tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+                tasks_response.append(task.to_json())
 
-    tasks_response = []
-    for task in tasks:
-            tasks_response.append(task.to_json())
+        return jsonify(tasks_response)
 
-    return jsonify(tasks_response)
-
-
+#wave2
 @tasks_bp.route("/<task_id>", methods=["GET","PUT","DELETE","PATCH"])
 def handle_task(task_id):
 
@@ -60,7 +53,6 @@ def handle_task(task_id):
 
     if request.method == "GET":
         return make_response({"task":task.to_json()},200)
-
     elif request.method == "PUT":
         form_data = request.get_json()
         task.title = form_data["title"]
@@ -70,7 +62,6 @@ def handle_task(task_id):
         db.session.commit()
 
         return make_response({"task":task.to_json()},200)
-
     elif request.method == "DELETE":
         db.session.delete(task)
         db.session.commit()
@@ -78,13 +69,15 @@ def handle_task(task_id):
         return make_response({
                     "details": f'Task {task.task_id} "{task.title}" successfully deleted'})
 
-
-@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
-def mark_completed(task_id):
+#wave 3 & 4
+@tasks_bp.route("/<task_id>/<mark_status>", methods=["PATCH"])
+def mark_completed(task_id,mark_status):
 
     task = Task.query.get(task_id)
 
-    if task:
+    if not task:
+        return make_response(" ", 404)
+    elif mark_status == "mark_complete":
         task.completed_at = datetime.utcnow()
         db.session.commit()
         an_external_web_api_url = "https://slack.com/api/chat.postMessage"
@@ -98,51 +91,37 @@ def mark_completed(task_id):
             
         }
         response = requests.post(an_external_web_api_url, params=task_params, headers=my_headers )
-        print(response)
         
-        return make_response({"task":response.json()},200)
+        return make_response({"task":task.to_json()},200)
 
-    else:
-        return make_response(" ", 404)
-    
-
-@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
-def mark_incompleted(task_id):
-
-    task = Task.query.get(task_id)
-
-    if task:
+    elif mark_status == "mark_incomplete":
         task.completed_at = None
         db.session.commit()
         return make_response({"task":task.to_json()},200)
-    else:
-        return make_response(" ", 404)
 
-
-@goals_bp.route("", methods=["POST"])
+#wave5
+@goals_bp.route("", methods=["POST","GET"])
 def handle_goals():
-
-    request_body = request.get_json()
     
-    if "title" not in request_body: 
-        return ({
-            "details":"Invalid data"
-        },400)
+    if request.method == "POST":
+
+        request_body = request.get_json()
+        
+        if "title" not in request_body: 
+            return ({
+                "details":"Invalid data"
+            },400)
+        else:
+            goal = Goal(title=request_body["title"])
+            db.session.add(goal)
+            db.session.commit()
+            return make_response({"goal":goal.goals_to_json()},201)
     else:
-        goal = Goal(title=request_body["title"])
-        db.session.add(goal)
-        db.session.commit()
-        return make_response({"goal":goal.goals_to_json()},201)
-
-
-@goals_bp.route("", methods=["GET"])
-def get_all_goal():
-    
-    goals = Goal.query.all()
-    goals_response = []
-    for goal in goals:
-            goals_response.append(goal.goals_to_json())
-    return jsonify(goals_response)
+        goals = Goal.query.all()
+        goals_response = []
+        for goal in goals:
+                goals_response.append(goal.goals_to_json())
+        return jsonify(goals_response)
 
 
 @goals_bp.route("/<goal_id>", methods=["GET","PUT","DELETE","PATCH"])
@@ -167,31 +146,34 @@ def handle_goal(goal_id):
         return make_response({
                     "details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'})
 
-@goals_bp.route("/<goal_id>/tasks",methods=["POST"])
+#wave6
+@goals_bp.route("/<goal_id>/tasks",methods=["POST","GET"])
 def create_goals_tasks(goal_id):
-    request_body = request.get_json()
-    num = int(goal_id)
-    for task_id in request_body["task_ids"]:
-        task = Task.query.get(task_id)
-        task.goal_id = num
-    db.session.commit()
-    return {
-        "id":num,
-        "task_ids":request_body["task_ids"]}
-
-@goals_bp.route("/<goal_id>/tasks",methods=["GET"])
-def get_goals_tasks(goal_id):
-    goal = Goal.query.get(goal_id)
-    if not goal:
-        return make_response("",404)
-    else:
+    if request.method=="POST":
+        request_body = request.get_json()
         num = int(goal_id)
-        tasks = Task.query.filter_by(goal_id=num)
-        tasks_list =[]
-        for task in tasks:
-            tasks_list.append(task.to_json())
+        for task_id in request_body["task_ids"]:
+            task = Task.query.get(task_id)
+            task.goal_id = num
+        db.session.commit()
+        return {
+            "id":num,
+            "task_ids":request_body["task_ids"]}
+    else:
+        goal = Goal.query.get(goal_id)
+        if not goal:
+            return make_response("",404)
+        else:
+            num = int(goal_id)
+            tasks = Task.query.filter_by(goal_id=num)
+            tasks_list =[]
+            for task in tasks:
+                tasks_list.append(task.to_json())
 
-        return {"id":num,"title":goal.title,"tasks":tasks_list},200
+            return {"id":num,"title":goal.title,"tasks":tasks_list},200
+
+
+    
 
 
 
