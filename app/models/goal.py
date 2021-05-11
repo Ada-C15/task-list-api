0b024c33fd1f75db50771ca48930cae339987a76
Delia <deliagaiaglow@gmail.com>
2021-05-10 22:17:10 -0700

    In case power outage - midway through wave 5

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,4 +3,14 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+
+    __tablename__= "goals"
+
+    def to_dict(self):
+        
+        return {
+            "id": self.id,
+            "title": self.title
+            }
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import db
 from flask import request, Blueprint, jsonify
 from .models.task import Task
+from .models.goal import Goal
 import datetime
 
 
-### WAVES 1 AND 2
+### WAVES 1 AND 2 - CRUD for TASKS
 
 # Create an endpoint specifically for tasks (all the routes start with /tasks):
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -16,7 +17,7 @@ def is_int(value):
         return False
 
 
-# define a route with default empty string for GET all tasks:
+# define a route with default empty string to GET all tasks:
 @tasks_bp.route("", methods=["GET"], strict_slashes=False)
 def get_tasks():
     sort_query = request.args.get("sort")
@@ -137,7 +138,7 @@ def delete_single_task(task_id):
     return jsonify({"details": f'Task {task.id} "{task.title}" successfully deleted'}), 200
 
 
-### WAVE 3 - PATCH ROUTES
+### WAVE 3 - PATCH ROUTES for TASKS
 
 # define a new route to update (PATCH) one task by its id (route parameter between <>, treated as a variable):
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"], strict_slashes=False)
@@ -178,11 +179,70 @@ def mark_incomplete(task_id):
         task.is_complete = False
 
     db.session.commit()
-
+                                                                                                                                                                    
     task_dict = task.to_dict()
         
     return jsonify({"task": task_dict}), 200
 
 
 
+### WAVE 5 - CRUD for GOALS
 
+# Create an endpoint specifically for goals (all the routes start with /goals):
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+def is_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return False
+
+# define a route with default empty string to GET all goals:
+@goals_bp.route("", methods=["GET"], strict_slashes=False)
+def get_goals():
+
+    goals = Goal.query.all() 
+    # build list that has all the goals in it:
+    goals_response = []
+    for goal in goals:
+        goals_response.append(goal.to_json())   
+
+    return jsonify(goals_response), 200
+
+
+# define a route with default empty string for POST:
+@goals_bp.route("", methods=["POST"], strict_slashes=False)
+def create_goal():  
+
+    request_body = request.get_json()
+
+    # if not request_body["title"]:  # triggers key error
+    if not request_body.get("title"):
+        return jsonify({"details": "Invalid data"}), 400 
+ 
+    new_goal = Goal(title = request_body["title"])
+
+    db.session.add(new_goal)   
+    db.session.commit()  
+
+    # we need an extra step to transfer completed_at to is_completed:
+    goal_dict = new_goal.to_dict()
+
+    return jsonify({"task": goal_dict}), 201
+
+
+# define a new route to GET a specific goal
+@goals_bp.route("/<goal_id>", methods=["GET"], strict_slashes=False)
+def get_one_goal(goal_id):
+
+    if not is_int(goal_id):        
+        return {"message": f"ID {goal_id} must be an integer"}, 400
+    
+    goal = Goal.query.get(goal_id)
+
+    if goal is None:
+        return jsonify(None), 404
+    else:
+        goal_dict = goal.to_dict()
+     
+    return jsonify({"goal": goal_dict}), 200
