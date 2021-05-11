1683a1cf7d62d4d17dc04f68bf9d96dc0c065df0
alison <alimcginley@gmail.com>
2021-05-11 09:59:07 -0700

    refactoring phase 1. Breaks methods into individual functions

--- app/routes.py
+++ app/routes.py
@@ -13,56 +13,69 @@ load_dotenv()
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals") 
 
-@tasks_bp.route("", methods = ["GET", "POST"])
-def handle_tasks():
+@tasks_bp.route("", methods = ["GET"])
+def get_tasks():
     tasks_response = []
-    if request.method == "GET":
-        order = request.args.get("sort")
-        if order == "asc":
-            tasks_query = Task.query.order_by(Task.title)
-        else:
-            tasks_query = Task.query.order_by(Task.title.desc())
-        for task in tasks_query:
-            tasks_response.append(task.build_dict())
-        return jsonify(tasks_response), 200
+    order = request.args.get("sort")
+    if order == "asc":
+        tasks_query = Task.query.order_by(Task.title)
+    else:
+        tasks_query = Task.query.order_by(Task.title.desc())
+    for task in tasks_query:
+        tasks_response.append(task.build_dict())
+    return jsonify(tasks_response), 200
 
-    elif request.method == "POST":
-        request_body = request.get_json()
-        if "title" not in request_body.keys() or "description" not in request_body.keys() or "completed_at" not in request_body.keys():
-            return make_response({"details": "Invalid data"}, 400)
+
+@tasks_bp.route("", methods = ["POST"])
+def add_tasks():
+    request_body = request.get_json()
+    if "title" not in request_body.keys() or "description" not in request_body.keys() or "completed_at" not in request_body.keys():
+        return make_response({"details": "Invalid data"}, 400)
         
-        new_task = Task(
-            title=request_body["title"],
-            description = request_body["description"],
-            completed_at = request_body["completed_at"],
-        )
-        slack_message(f"Someone just added {new_task.title} to the task list.")
-        db.session.add(new_task)
-        db.session.commit()
+    new_task = Task(
+        title=request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"],
+    )
+    slack_message(f"Someone just added {new_task.title} to the task list.")
+    db.session.add(new_task)
+    db.session.commit()
 
-        return {"task":new_task.build_dict()}, 201
+    return {"task":new_task.build_dict()}, 201
 
-@tasks_bp.route("/<task_id>", methods = ["GET", "PUT", "DELETE", "PATCH"])
-def handle_task(task_id):
+
+@tasks_bp.route("/<task_id>", methods = ["GET"])
+def get_task(task_id):
     task = Task.query.get(task_id)
     if task is None:
         return make_response("", 404)
-    if request.method == "GET":
-        return make_response(jsonify({"task":task.build_dict()}))
-    elif request.method == "PUT":
-        form_data = request.get_json()
-        task.title = form_data["title"]
-        task.description = form_data["description"]
-        task.completed_at = form_data["completed_at"]
 
-        db.session.commit()
+    return make_response(jsonify({"task":task.build_dict()}))
+        
 
-        return make_response(jsonify({"task":task.build_dict()}))
-    elif request.method == "DELETE":
-        db.session.delete(task)
-        db.session.commit()
+@tasks_bp.route("/<task_id>", methods = ["PUT"])
+def update_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return make_response("", 404)
+    form_data = request.get_json()
+    task.title = form_data["title"]
+    task.description = form_data["description"]
+    task.completed_at = form_data["completed_at"]
 
-        return {"details": f"Task {task_id} \"{task.title}\" successfully deleted"}
+    db.session.commit()
+
+    return make_response(jsonify({"task":task.build_dict()}))
+
+@tasks_bp.route("/<task_id>", methods = ["DELETE"])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return make_response("", 404)
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details": f"Task {task_id} \"{task.title}\" successfully deleted"}
     
 @tasks_bp.route("/<task_id>/mark_complete", methods = ["PATCH"])
 def mark_complete(task_id):
