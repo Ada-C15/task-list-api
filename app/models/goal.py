1e13696ff37553173efa45482b0448d191023620
Abigail Charime <abigail.charime@gmail.com>
2021-05-12 15:37:43 -0700

    Added bot notification functionality, Added POST, GET, DELETE, and PUT functionality to driver code, and added one to many rel to goal and task model/Wave 4,5,(a bit of 6)

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,6 +1,18 @@
 from flask import current_app
+from sqlalchemy.orm import backref
 from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    tasks = db.relationship('Task', backref='goal', lazy=True)
+
+
+    def to_json_goal(self):
+        # This method was created so that we do not have to write out the dictionary many times in the routes.py file. 
+        return {
+            "id": self.goal_id,
+            "title": self.title,
+        }
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from werkzeug.datastructures import Authorization
 from app import db
 from app.models.task import Task
 from flask import Blueprint, request, jsonify
 from datetime import datetime
+import os
+import requests
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -81,9 +84,29 @@ def mark_complete(task_id):
         return jsonify(None), 404
     # Mark Complete on an Incompleted Task
     patch_task.completed_at = date
+    bot_notification(patch_task)
     db.session.commit()
-
+    
     return {"task":patch_task.to_json()}, 200
+    
+
+def bot_notification(patch_task):
+    # notification_task = Task.query.get(task_id)
+    PATH = "https://slack.com/api/chat.postMessage"
+    API_TOKEN = os.environ.get("API_KEY")
+
+    query_params = {
+        "channel": "task-notifications",
+        "text": f"Someone just completed the task {patch_task.title}"
+    }
+
+    header = {
+        "Authorization": f"Bearer {API_TOKEN}"
+
+    }
+    result = requests.get(PATH, params=query_params,headers=header)
+
+    return result
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_incomplete(task_id):
@@ -94,3 +117,72 @@ def mark_incomplete(task_id):
     db.session.commit()
 
     return {"task":patch_task.to_json()}, 200
+
+
+# ============================Goals=========================================
+
+from app.models.goal import Goal
+from flask import Blueprint, request, jsonify
+
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+@goals_bp.route("",methods=["POST"])
+def create_goal():
+    try:
+        # This portion is the POST request
+        request_body = request.get_json()
+        new_goal = Goal(title=request_body["title"])
+
+        db.session.add(new_goal)
+        db.session.commit()
+        return {"goal": new_goal.to_json_goal()}, 201
+    except KeyError:
+        return {
+            "details": "Invalid data"}, 400
+@goals_bp.route("", methods=["GET"])
+def goals_index():
+    # This portion is the just a GET request
+    goals = Goal.query.all()
+    if goals == None:
+        return []
+    else:
+        goals_response = []
+        for goal in goals:
+            goals_response.append(goal.to_json_goal())
+        return jsonify(goals_response), 200
+
+@goals_bp.route("/<goal_id>", methods=["GET","PUT", "DELETE"])
+def get_single_goal(goal_id):
+
+    goal = Goal.query.get(goal_id)
+    # With the GET, POST and DELETE request if there is nothing we output this
+    if request == None or goal == None:
+        return jsonify(None), 404
+    # This portion is the GET request for only one task
+    elif request.method == "GET":
+        return {"goal": goal.to_json_goal()}, 200
+    elif request.method == "PUT":
+        # This portion is the PUT request for only one task
+        request_body = request.get_json()
+        goal.title = request_body["title"]
+        # Save action
+        db.session.commit()
+        return {"goal": goal.to_json_goal()}, 200
+    elif request.method == "DELETE":
+        db.session.delete(goal)
+        db.session.commit()
+        return {
+            "details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"
+            }, 200
+
+
+# # ===============Establishing One to Many Realtionship=================================
+
+# @goals_bp.route("goals/<goal_id>/tasks", method=["POST"])
+# def send_task_ids():
+#     request_body = request.get_json()
+#     add_tasks = Goal(tasks=request_body["task_ids"])
+
+#     db.session.add(add_tasks)
+#     db.session.commit()
+#     return { "id": "goal_id", "task_ids": add_tasks}, 201
