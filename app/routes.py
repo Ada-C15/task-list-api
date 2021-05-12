353a7d808ac1f6b247bd54dd6849a1d0bda5e3e4
Lin Zhang <linzhang294@gmail.com>
2021-05-11 23:33:01 -0400

    passes wave 5 tests

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,12 @@ from datetime import datetime
 from flask import request, Blueprint, make_response, jsonify
 import requests
 import os
-# from app.models.goal import Goal
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+from app.models.goal import Goal
+goals_bp = Blueprint(
+    "goals", __name__, url_prefix="/goals")
 
 tasks_bp = Blueprint(
     "tasks", __name__, url_prefix="/tasks")
@@ -86,7 +88,7 @@ def handle_incomplete(task_id):
     else:
         task.completed_at = None
         db.session.commit()
-        return jsonify({"task": task.to_dict()}),   200
+        return jsonify({"task": task.to_dict()}), 200
 
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"], strict_slashes=False)
@@ -131,3 +133,46 @@ def call_slack_api(task):
 # -------------------------
 # WAVE 5 - GOAL ENDPOINTS
 # -------------------------
+
+@goals_bp.route("", methods=["POST"], strict_slashes=False)
+def create_goal():
+    request_body = request.get_json()
+    if "title" in request_body:
+        new_goal = Goal(
+            title=request_body["title"])
+        db.session.add(new_goal)
+        db.session.commit()
+        return jsonify({"goal": new_goal.to_dict()}), 201
+    return make_response({"details": "Invalid data"}, 400)
+
+
+@goals_bp.route("", methods=["GET"], strict_slashes=False)
+def goal_index():
+    goals = Goal.query.all()
+    goals_response = []
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+    return make_response(jsonify(goals_response), 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+def handle_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    if goal is None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        return jsonify({"goal": goal.to_dict()}), 200
+
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        goal.title = form_data["title"]
+        db.session.commit()
+        return jsonify({"goal": goal.to_dict()}), 200
+
+    elif request.method == "DELETE":
+        db.session.delete(goal)
+        db.session.commit()
+        goal_response = {
+            "details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}
+        return make_response(goal_response), 200
