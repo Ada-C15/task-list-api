2f836fee11516f7a2ae2e91a261af4b1ac677e52
Ari <ari.go.org.7@gmail.com>
2021-05-12 12:23:31 -0700

    added configuration change for json keys format and set to false

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,8 @@ load_dotenv()   # calls variables (that are text) from .ennv and converts
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # to keep order of sorted dictionary passed to jsonify() function
+    app.config['JSON_SORT_KEYS'] = False 
 
     if test_config is None: 
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
