0f42ef8285e97c1dffb160dca4a21a2ab25e6f04
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-12 21:01:07 -0700

    sets up one-to-many relationship btw Goal & Task

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,10 +1,12 @@
 from flask import current_app
+from sqlalchemy.orm import backref
 from app import db
 
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    task = db.relationship('Task', backref='goaltask', lazy=True)
 
     def goal_json(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,14 @@
 from flask import current_app
 from app import db
 from sqlalchemy import DateTime
+from app.models.goal import Goal
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goaltask_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
 
     # creates a dictionary of key-value pairs describing the given task
     def to_json(self):
