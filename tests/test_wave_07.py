5b1097aad727132143c91cf09fca3c3c8fa523f8
Ruthie Newman <ruthienewman@protonmail.com>
2021-05-13 09:47:08 -0700

    Comments out test_wave_07

--- tests/test_wave_07.py
+++ tests/test_wave_07.py
@@ -1,69 +1,69 @@
-#test to get tasks sorted by id
-def test_get_tasks_sorted_by_id(client, three_tasks):
-    # Act
-    response = client.get("/tasks?sort=id")
-    response_body = response.get_json()
+# #test to get tasks sorted by id
+# def test_get_tasks_sorted_by_id(client, three_tasks):
+#     # Act
+#     response = client.get("/tasks?sort=id")
+#     response_body = response.get_json()
 
-    # Assert
-    assert response.status_code == 200
-    assert len(response_body) == 3
-    assert response_body == [
-        {
-            "description": "",
-            "id": 1,
-            "is_complete": False,
-            "title": "Water the garden 🌷"},
-        {
-            "description": "",
-            "id": 2,
-            "is_complete": False,
-            "title": "Answer forgotten email 📧"},
-        {
-            "description": "",
-            "id": 3,
-            "is_complete": False,
-            "title": "Pay my outstanding tickets 😭"}
-    ]
+#     # Assert
+#     assert response.status_code == 200
+#     assert len(response_body) == 3
+#     assert response_body == [
+#         {
+#             "description": "",
+#             "id": 1,
+#             "is_complete": False,
+#             "title": "Water the garden 🌷"},
+#         {
+#             "description": "",
+#             "id": 2,
+#             "is_complete": False,
+#             "title": "Answer forgotten email 📧"},
+#         {
+#             "description": "",
+#             "id": 3,
+#             "is_complete": False,
+#             "title": "Pay my outstanding tickets 😭"}
+#     ]
     
 
-#Tests for goals sorted by title asc/desc
-def test_get_goals_sorted_asc(client, three_goals):
-    # Act
-    response = client.get("/goals?sort=asc")
-    response_body = response.get_json()
+# #Tests for goals sorted by title asc/desc
+# def test_get_goals_sorted_asc(client, three_goals):
+#     # Act
+#     response = client.get("/goals?sort=asc")
+#     response_body = response.get_json()
 
-    # Assert
-    assert response.status_code == 200
-    assert len(response_body) == 3
-    assert response_body == [
-        {   
-            "id" : 2,
-            "title": "Eat healthy"},
-        {   "id" : 3, 
-            "title": "Get better sleep"}, 
-        {   
-            "id" : 1,
-            "title": "Get more excercise"}
-    ]
+#     # Assert
+#     assert response.status_code == 200
+#     assert len(response_body) == 3
+#     assert response_body == [
+#         {   
+#             "id" : 2,
+#             "title": "Eat healthy"},
+#         {   "id" : 3, 
+#             "title": "Get better sleep"}, 
+#         {   
+#             "id" : 1,
+#             "title": "Get more excercise"}
+#     ]
 
 
-#Test for getting a response body with goals sorted by title
-def test_get_goals_sorted_desc(client, three_goals):
-    # Act
-    response = client.get("/goals?sort=desc")
-    response_body = response.get_json()
+# #Test for getting a response body with goals sorted by title
+# def test_get_goals_sorted_desc(client, three_goals):
+#     # Act
+#     response = client.get("/goals?sort=desc")
+#     response_body = response.get_json()
 
-    # Assert
-    assert response.status_code == 200
-    assert len(response_body) == 3
-    assert response_body == [
-        {   
-            "id" : 1,
-            "title": "Get more excercise"}, 
-        {   "id" : 3, 
-            "title": "Get better sleep"},
-        {   
-            "id" : 2,
-            "title": "Eat healthy"} 
-    ]
+#     # Assert
+#     assert response.status_code == 200
+#     assert len(response_body) == 3
+#     assert response_body == [
+#         {   
+#             "id" : 1,
+#             "title": "Get more excercise"}, 
+#         {   "id" : 3, 
+#             "title": "Get better sleep"},
+#         {   
+#             "id" : 2,
+#             "title": "Eat healthy"} 
+#     ]
 
