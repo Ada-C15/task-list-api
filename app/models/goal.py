66b5e8d8943f230aa140683910122c89f3a3a42e
Lacy Draper <ldrape1@gmail.com>
2021-05-13 21:06:52 -0700

    added docstrings

--- app/models/goal.py
+++ app/models/goal.py
@@ -7,14 +7,18 @@ class Goal(db.Model):
     title = db.Column(db.String)
     # whatever I put in the backref in creates a fake column in the Task table noted in the argument before backref?
     #tasks is going to be a list of Task instances. its called an iterator 
+    #lazy= true  means that it will return all data assigned to the object even if you use a key as a referen
     tasks = db.relationship("Task", backref='task', lazy=True)
     
-    #helper function to return in json format
+    
     def goal_to_json(self):
-        
+        """
+            Input:  instance of Goal
+            Output: returns python dictionary of instance of Goal
+        """
         return {
-        "id": self.goal_id,
-        "title": self.title,
+            "id": self.goal_id,
+            "title": self.title,
         }
     
     
--- app/routes.py
+++ app/routes.py
@@ -9,16 +9,21 @@ import os
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 goal_bp = Blueprint("goal", __name__, url_prefix="/goals")
 
-#endpoint for new task entry post method 
-@task_bp.route("", methods=["POST"])
+
+@task_bp.route("", methods=["POST"], strict_slashes=False)
 def create_task():
+    """
+        Input: Request to create a new task
+        Output: Returns an error if request body is missing attributes, otherwise, the new task
+                and its value is returned in json format
+    """
+    
     request_body = request.get_json()
     #checks to see if 'title' is found as a key in request_body called 'validation'
     if 'title' not in request_body:
         return make_response(jsonify({"details":"Invalid data"}), 400)
     if 'description' not in request_body:
         return make_response(jsonify({"details":"Invalid data"}), 400)
-    # checking to see if key exists in the dictionary
     if 'completed_at' not in request_body:
         return make_response(jsonify({"details":"Invalid data"}), 400)
     # checking to see what the value is in the completed_at key 
@@ -44,8 +49,14 @@ def create_task():
 
 
 #endpoint for getting tasks
-@task_bp.route("", methods=["GET"])
+@task_bp.route("", methods=["GET"], strict_slashes=False)
 def tasks():
+    """
+        Input: Request to query all saved tasks. handles query parameter 'sort' if given 
+        Output: Returns an empty list if there are no saved tasks, returns a json dictionary of all 
+                saved tasks dictionaries
+    
+    """
     
     # grabbing sort query parameter and its value ex. sort=asc from the client request and storing it in local variable
     sort_titles = request.args.get('sort')
@@ -62,7 +73,6 @@ def tasks():
             task_by_desc = Task.query.order_by(Task.title.desc())
             for task in task_by_desc:
                 task_response.append(task.to_json())
-        
         return jsonify(task_response), 200
     
     else:
@@ -74,9 +84,15 @@ def tasks():
         return jsonify(task_response), 200
 
 
-#endpoint to get response body by task_id
-@task_bp.route("/<task_id>", methods=["GET","PUT","DELETE"],)
+
+@task_bp.route("/<task_id>", methods=["GET","PUT","DELETE"],strict_slashes=False)
 def task(task_id):
+    """
+        Input: Request to read, edit, or delete a task by a given task_id
+        Output: Returns an error if the task does not exist, returns dictionary of Task instance 
+                along with OK 200 code
+    """
+    
     task = Task.query.get(task_id)
     # if task is not found
     if not task:
@@ -89,7 +105,7 @@ def task(task_id):
         else:
             return {"task":task.to_json()},200
     
-    if request.method =='PUT':
+    if request.method == 'PUT':
         request_body = request.get_json()
         #assigning updated values to a task instance
         task.title = request_body['title']
@@ -104,24 +120,32 @@ def task(task_id):
         return jsonify(details=f'Task {task.task_id} "{task.title}" successfully deleted'),200
 
 
-    
 
-#endpoint for giving completed_at attribute column with a datetime value in the DB. route parameter is mark_complete
-@task_bp.route("<task_id>/mark_complete",methods=["PATCH"])
+@task_bp.route("<task_id>/mark_complete",methods=["PATCH"], strict_slashes=False)
 def complete(task_id):
+    """
+        Input:  Request to mark a task complete with mark_complete in the path
+        Output: Returns an error if task does not exist, marks task completed with a datetime
+                datatype and returns updated task dictionary  
+    """
+    
     task = Task.query.get(task_id)
     if not task:
         return '', 404 
-    # in order to make variable truthy we need to have datatype as value
+    #in order to make variable truthy we need to have datatype as value
     task.completed_at = datetime.utcnow()
     db.session.commit()
-    
+    # calling helper function
     call_slack(task)
     
     return make_response({"task": task.to_json()}, 200)#helper function to 
 
-#call slack api helper function    
+ 
 def call_slack(task):
+    """
+        Input: Task instance
+        Output: Returns a post request to designated URL
+    """
     #using os package from __init__.py to pull value for the argument passed in 
     key = os.environ.get("API_KEY")
     url = "https://slack.com/api/chat.postMessage"
@@ -130,10 +154,13 @@ def call_slack(task):
     
 
 
-
-#endpoint for giving completed_at column/attribute the value null/NONE in the DB. route parameter is mark_incomplete
-@task_bp.route("<task_id>/mark_incomplete",methods=["PATCH"])
+@task_bp.route("<task_id>/mark_incomplete",methods=["PATCH"],strict_slashes=False)
 def in_complete(task_id):
+    """
+        Input:  Request with task_id with mark_incomplete as an argument
+        Output: Returns error if task does not exist, returns updated instance of task dictionary 
+                with updated completed_at attribute value set to null/NONE
+    """
     task = Task.query.get(task_id)
     if not task:
         return '', 404    
@@ -141,9 +168,16 @@ def in_complete(task_id):
     db.session.commit()
     return make_response({"task": task.to_json()}, 200)
 
-#endpoint for creating a new goal
-@goal_bp.route("",methods=["POST"])
+
+
+@goal_bp.route("",methods=["POST"], strict_slashes=False)
 def create_goal():
+    """
+        Input: Request to create a new goal instance
+        Output: Returns an error if request body is missing attributes, otherwise, goal instance
+                and its value is returned in json format
+    """
+    
     request_body = request.get_json()
     #checks to see if 'title' is found as a key in request_body called 'validation'
     if 'title' not in request_body:
@@ -158,23 +192,26 @@ def create_goal():
     "goal": new_goal.goal_to_json()
     }), 201
 
-#endpont for GET, PUT, DELETE based on goal_id
-@goal_bp.route("/<goal_id>", methods=["GET","PUT","DELETE"],)
+
+@goal_bp.route("/<goal_id>", methods=["GET","PUT","DELETE"],strict_slashes=False)
 def handle_goal(goal_id):
+    """
+        Input: Request to read, edit, or delete a goal by goal_id
+        Output: Returns an error if the goal does not exist, returns dictionary of Goal instance 
+                along with OK 200 code
+    """
+    
     goal = Goal.query.get(goal_id)
-    # if task is not found
     if not goal:
         return '',404
-    #
+    
     if request.method == "GET":
         return jsonify(goal=goal.goal_to_json()), 200
    
-    
     if request.method =='PUT':
         request_body = request.get_json()
         #assigning updated values to task
         goal.title = request_body['title']
-    
         db.session.commit()
         return jsonify(goal=goal.goal_to_json()), 200
 
@@ -183,9 +220,14 @@ def handle_goal(goal_id):
         db.session.commit()
         return jsonify(details=f'Goal {goal.goal_id} "{goal.title}" successfully deleted'),200
 
-# GET method to return all goals
-@goal_bp.route('', methods=["GET"])
+
+@goal_bp.route('', methods=["GET"],strict_slashes=False)
 def get_goals():
+    """
+        Input: Request to query all saved instances of Goal. 
+        Output: Returns an empty list if there are no saved goals, returns a json dictionary of all 
+                saved goal dictionaries
+    """
     goals = Goal.query.all()
     goals_response = []
     for goal in goals:
@@ -194,39 +236,44 @@ def get_goals():
 
 
 
-# POST endpoint to add list of task_ids to one goal
-@goal_bp.route("/<goal_id>/tasks",methods=["POST"])
+@goal_bp.route("/<goal_id>/tasks",methods=["POST"], strict_slashes=False)
 def tasks_to_goal(goal_id):
+    """
+        Input:  Request to add a list of task id's to an instance of goal by its goal_id
+        Output: Returns error if goal does not exist, returns a dictionary of the goal instance by its
+                goad_id and the task_ids associated with the goal
+    """
     goal = Goal.query.get(goal_id)
     if not goal:
         return '',404
-    # request body has a goal and list of tasks to assign to that goal
+    # request body has a goal and list of tasks to assigned to that goal
     request_body = request.get_json()
     # request_body looks like {'tasks': [1,2]}  value of tasks refers to the task_id columm of Task 
-
     for task_id in request_body['task_ids']:
         task = Task.query.get(task_id)
         if task not in goal.tasks:
             goal.tasks.append(task)
-            
-    
     db.session.commit()
     
     return make_response({"id": int(goal_id), "task_ids": request_body["task_ids"]}, 200)                                                                          
 
-#GET route to return tasks of a specific goal                
-@goal_bp.route("/<goal_id>/tasks",methods=["GET"])
+              
+@goal_bp.route("/<goal_id>/tasks",methods=["GET"], strict_slashes=False)
 def get_goal_tasks(goal_id):
+    """
+        Input:  Request to get tasks of one goal by goal_id
+        Output: Returns error if goal does not exist, returns dictionary with goal_id 
+                goal_title, and list of tasks for the goal instance
+    """
+    
     goal = Goal.query.get(goal_id)
     if not goal:
         return '',404
-    # request body contains a goal object with given goal_id
     
     list_of_tasks = []
-    #tasks is referring to our tasks attribute in Goal#
+    #tasks is referring to our tasks attribute in goal instance
+    # we are using the backref "tasks" in Goal model for goal.tasks
     for task in goal.tasks:
-        list_of_tasks.append(task.to_json_goal_id())
-        #goal.task(helper function with goal_id as a key)
-    
+        list_of_tasks.append(task.to_json_goal_id())#goal.task(helper function with goal_id as a key)
     return jsonify(id=int(goal_id),title=goal.title,tasks=list_of_tasks), 200
 
